9b54d412730c397a8668111784c62d9dd0e30549
andy.padavan <andy.padavan@gmail.com>
2012-07-22 01:50:58 +0800

    transmission: upstream updates

--- trunk/user/transmission/transmission-2.60/libtransmission/version.h
+++ trunk/user/transmission/transmission-2.60/libtransmission/version.h
@@ -1,9 +1,9 @@
 #define PEERID_PREFIX             "-TR2600-"
 #define USERAGENT_PREFIX          "2.60"
-#define SVN_REVISION              "13375"
-#define SVN_REVISION_NUM          13375
+#define SVN_REVISION              "13392"
+#define SVN_REVISION_NUM          13392
 #define SHORT_VERSION_STRING      "2.60"
-#define LONG_VERSION_STRING       "2.60 (13375)"
+#define LONG_VERSION_STRING       "2.60 (13392)"
 #define VERSION_STRING_INFOPLIST  2.60
 #define MAJOR_VERSION             2
 #define MINOR_VERSION             60

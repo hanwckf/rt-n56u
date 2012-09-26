650bf4410c61d84443c3c5622ae01fd369849ee3
andy.padavan <andy.padavan@gmail.com>
2012-09-27 02:11:01 +0800

    www: always enabled buttons update and pause for wan status

--- trunk/user/www/n56u_ribbon_fixed/detectWAN.js
+++ trunk/user/www/n56u_ribbon_fixed/detectWAN.js
@@ -7,32 +7,14 @@ var lan_subnet_t = "";
 var detect_if_wan = 0;
 var manually_disconnect_wan = 0;
 
-function refresh_waninfo(){	
-	if(link_internet == "0"){
-		//$("#connectstatus")[0].innerHTML = "<#Connected#>";
-		//$("#connectbutton")[0].value = "<#Disconnect#>";
-		document.internetForm.connectbutton_link.disabled = false;
-		document.internetForm.connectbutton_nolink.disabled = true;	
-		//parent.showMapWANStatus(1);
-	}
-	else if(link_internet == "1"){
-		//$("#connectstatus")[0].innerHTML = "<#Disconnected#>";
-		//$("#connectbutton")[0].value = "<#Connect#>";
-		document.internetForm.connectbutton_link.disabled = true;	
-		document.internetForm.connectbutton_nolink.disabled = false;	
-		//parent.showMapWANStatus(0);
-	}
-	else{
-		document.internetForm.connectbutton_link.disabled = true;	
-		document.internetForm.connectbutton_nolink.disabled = true;	
-	}
-	//$("#connectbutton")[0].style.display = "";
+function refresh_waninfo(){
+	document.internetForm.connectbutton_link.disabled = false;
+	document.internetForm.connectbutton_nolink.disabled = false;
 }
 
 function detectWANstatus(){
-
-    var $j = jQuery.noConflict();
-    $j.ajax({
+	var $j = jQuery.noConflict();
+	$j.ajax({
 		url: '/WAN_info.asp',
 		dataType: 'script',
 		

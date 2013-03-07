ffcb200a88c98fbd3c3d4fdb90ffc1c5cb7bce3a
andy.padavan <andy.padavan@gmail.com>
2013-03-08 02:59:32 +0800

    kernel-3.0.x: tcp: avoid wakeups for pure ACK, commit 79ffef1fe213851f44bfccf037170a140e929f85 from upstream

--- trunk/linux-3.0.x/include/net/tcp.h
+++ trunk/linux-3.0.x/include/net/tcp.h
@@ -902,6 +902,10 @@ static inline int tcp_prequeue(struct sock *sk, struct sk_buff *skb)
 	if (sysctl_tcp_low_latency || !tp->ucopy.task)
 		return 0;
 
+	if (skb->len <= tcp_hdrlen(skb) &&
+	    skb_queue_len(&tp->ucopy.prequeue) == 0)
+		return 0;
+
 	__skb_queue_tail(&tp->ucopy.prequeue, skb);
 	tp->ucopy.memory += skb->truesize;
 	if (tp->ucopy.memory > sk->sk_rcvbuf) {

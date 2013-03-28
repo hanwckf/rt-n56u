24325ed4056e443169ec26861705b5d031e151c3
andy.padavan <andy.padavan@gmail.com>
2013-03-28 19:18:40 +0800

    dropbear: update to v2013.56

--- trunk/user/dropbear/sshd.sh
+++ trunk/user/dropbear/sshd.sh
@@ -4,6 +4,10 @@ dir_storage="/etc/storage"
 
 func_start()
 {
+	key_s=""
+	key_x="-x"
+	key_4=""
+	
 	[ ! -d "$dir_storage" ] && mkdir -p $dir_storage
 	
 	if [ ! -f "$dir_storage/dropbear_rsa_host_key" ] ; then
@@ -15,10 +19,20 @@ func_start()
 	fi
 	
 	if [ -n "$1" ] ; then
-		/usr/sbin/dropbear -s -W 65536
-	else
-		/usr/sbin/dropbear -W 65536
+		key_s="-s"
 	fi
+	
+	db_kex_new=`nvram get db_kex_new`
+	if [ "$db_kex_new" == "1" ] ; then
+		key_x=""
+	fi
+	
+	ip6_service=`nvram get ip6_service`
+	if [ -z "$ip6_service" ] && [ -d /proc/sys/net/ipv6 ] ; then
+		key_4="-4"
+	fi
+	
+	/usr/sbin/dropbear $key_x $key_4 $key_s
 }
 
 func_stop()

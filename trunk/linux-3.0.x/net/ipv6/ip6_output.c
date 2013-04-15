9a0f66cf57f88fdeb1ad055beb5d2495a8cf2428
andy.padavan <andy.padavan@gmail.com>
2013-04-15 18:40:15 +0800

    kernel-3.0.x: ipv6: Don't send packet to big messages to self, commit f4e53e292ad0626a9d4f80cbdb6adae867d8b2bc from upstream

--- trunk/linux-3.0.x/net/ipv6/ip6_output.c
+++ trunk/linux-3.0.x/net/ipv6/ip6_output.c
@@ -280,10 +280,8 @@ int ip6_xmit(struct sock *sk, struct sk_buff *skb, struct flowi6 *fl6,
 			       dst->dev, dst_output);
 	}
 
-	if (net_ratelimit())
-		printk(KERN_DEBUG "IPv6: sending pkt_too_big to self\n");
 	skb->dev = dst->dev;
-	icmpv6_send(skb, ICMPV6_PKT_TOOBIG, 0, mtu);
+	ipv6_local_error(sk, EMSGSIZE, fl6, mtu);
 	IP6_INC_STATS(net, ip6_dst_idev(skb_dst(skb)), IPSTATS_MIB_FRAGFAILS);
 	kfree_skb(skb);
 	return -EMSGSIZE;

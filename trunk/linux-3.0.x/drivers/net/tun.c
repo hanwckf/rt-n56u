6a593c75a81c74e75136acb4d24e944cb4bda190
andy.padavan <andy.padavan@gmail.com>
2013-06-11 03:17:16 +0800

    kernel-3.0.x: tun: do not put self in waitq if doing a nonblock read, commit 61a5ff15ebdab87887861a6b128b108404e4706d from upstream.

--- trunk/linux-3.0.x/drivers/net/tun.c
+++ trunk/linux-3.0.x/drivers/net/tun.c
@@ -842,7 +842,8 @@ static ssize_t tun_do_read(struct tun_struct *tun,
 
 	tun_debug(KERN_INFO, tun, "tun_chr_read\n");
 
-	add_wait_queue(&tun->wq.wait, &wait);
+	if (unlikely(!noblock))
+		add_wait_queue(&tun->wq.wait, &wait);
 	while (len) {
 		current->state = TASK_INTERRUPTIBLE;
 
@@ -873,7 +874,8 @@ static ssize_t tun_do_read(struct tun_struct *tun,
 	}
 
 	current->state = TASK_RUNNING;
-	remove_wait_queue(&tun->wq.wait, &wait);
+	if (unlikely(!noblock))
+		remove_wait_queue(&tun->wq.wait, &wait);
 
 	return ret;
 }

a6133932111627998fee5ca80125595d3e701176
andy.padavan <andy.padavan@gmail.com>
2013-08-02 19:36:53 +0800

    rp-l2tp: enable support new kernels by default

--- trunk/user/rp-l2tp/rp-l2tp-0.5-rtn/handlers/sync-pppd.c
+++ trunk/user/rp-l2tp/rp-l2tp-0.5-rtn/handlers/sync-pppd.c
@@ -34,7 +34,7 @@ static char const RCSID[] =
 #define MAX_FDS 256
 
 /* Use management tunnel socket under 2.6.23+ */
-/* #define PPPOL2TP_V1 */
+#define PPPOL2TP_V1
 
 extern int pty_get(int *mfp, int *sfp);
 static int establish_tunnel(l2tp_tunnel *tun);

6552216b5e25e56ddb9b9c477742ca2adf53d1a6
andy.padavan <andy.padavan@gmail.com>
2013-08-31 04:39:12 +0800

    kernel-3.x.x: netfilter: xt_TCPMSS: Fix missing fragmentation handling, commit b396966c4688522863572927cb30aa874b3ec504 from upstream

--- trunk/linux-3.4.x/net/netfilter/xt_TCPMSS.c
+++ trunk/linux-3.4.x/net/netfilter/xt_TCPMSS.c
@@ -56,6 +56,10 @@ tcpmss_mangle_packet(struct sk_buff *skb,
 	u16 newmss;
 	u8 *opt;
 
+	/* This is a fragment, no TCP header is available */
+	if (par->fragoff != 0)
+		return 0;
+
 	if (!skb_make_writable(skb, skb->len))
 		return -1;
 

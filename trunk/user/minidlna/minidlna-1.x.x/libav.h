09a2ea54558c251891ffb716731faafd9ab93d36
andy.padavan <andy.padavan@gmail.com>
2014-03-16 18:27:27 +0800

    minidlna: update to v1.1.2+ (up to 0ea248)

---
+++ trunk/user/minidlna/minidlna-1.x.x/libav.h
@@ -0,0 +1,84 @@
+/* MiniDLNA media server
+ * Copyright (C) 2013  NETGEAR
+ *
+ * This file is part of MiniDLNA.
+ *
+ * MiniDLNA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * MiniDLNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiniDLNA. If not, see <http://www.gnu.org/licenses/>.
+ */
+#if HAVE_FFMPEG_LIBAVUTIL_AVUTIL_H
+#include <ffmpeg/libavutil/avutil.h>
+#elif HAVE_LIBAV_LIBAVUTIL_AVUTIL_H
+#include <libav/libavutil/avutil.h>
+#elif HAVE_LIBAVUTIL_AVUTIL_H
+#include <libavutil/avutil.h>
+#elif HAVE_FFMPEG_AVUTIL_H
+#include <ffmpeg/avutil.h>
+#elif HAVE_LIBAV_AVUTIL_H
+#include <libav/avutil.h>
+#elif HAVE_AVUTIL_H
+#include <avutil.h>
+#endif
+
+#if HAVE_FFMPEG_LIBAVCODEC_AVCODEC_H
+#include <ffmpeg/libavcodec/avcodec.h>
+#elif HAVE_LIBAV_LIBAVCODEC_AVCODEC_H
+#include <libav/libavcodec/avcodec.h>
+#elif HAVE_LIBAVCODEC_AVCODEC_H
+#include <libavcodec/avcodec.h>
+#elif HAVE_FFMPEG_AVCODEC_H
+#include <ffmpeg/avcodec.h>
+#elif HAVE_LIBAV_AVCODEC_H
+#include <libav/avcodec.h>
+#elif HAVE_AVCODEC_H
+#include <avcodec.h>
+#endif
+
+#if HAVE_FFMPEG_LIBAVFORMAT_AVFORMAT_H
+#include <ffmpeg/libavformat/avformat.h>
+#elif HAVE_LIBAV_LIBAVFORMAT_AVFORMAT_H
+#include <libav/libavformat/avformat.h>
+#elif HAVE_LIBAVFORMAT_AVFORMAT_H
+#include <libavformat/avformat.h>
+#elif HAVE_FFMPEG_AVFORMAT_H
+#include <ffmpeg/avformat.h>
+#elif HAVE_LIBAV_LIBAVFORMAT_H
+#include <libav/avformat.h>
+#elif HAVE_AVFORMAT_H
+#include <avformat.h>
+#endif
+
+#ifndef FF_PROFILE_H264_BASELINE
+#define FF_PROFILE_H264_BASELINE 66
+#endif
+#ifndef FF_PROFILE_H264_CONSTRAINED_BASELINE
+#define FF_PROFILE_H264_CONSTRAINED_BASELINE 578
+#endif
+#ifndef FF_PROFILE_H264_MAIN
+#define FF_PROFILE_H264_MAIN 77
+#endif
+#ifndef FF_PROFILE_H264_HIGH
+#define FF_PROFILE_H264_HIGH 100
+#endif
+#ifndef FF_PROFILE_SKIP
+#define FF_PROFILE_SKIP -100
+#endif
+
+#if LIBAVCODEC_VERSION_MAJOR < 53
+#define AVMEDIA_TYPE_AUDIO CODEC_TYPE_AUDIO
+#define AVMEDIA_TYPE_VIDEO CODEC_TYPE_VIDEO
+#endif
+
+#if LIBAVUTIL_VERSION_INT < ((50<<16)+(13<<8)+0)
+#define av_strerror(x, y, z) snprintf(y, z, "%d", x)
+#endif
+
---
+++ trunk/user/minidlna/minidlna-1.x.x/utils.h
@@ -0,0 +1,79 @@
+/* Utility functions
+ *
+ * Project : minidlna
+ * Website : http://sourceforge.net/projects/minidlna/
+ * Author  : Justin Maggard
+ *
+ * MiniDLNA media server
+ * Copyright (C) 2008-2009  Justin Maggard
+ *
+ * This file is part of MiniDLNA.
+ *
+ * MiniDLNA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * MiniDLNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiniDLNA. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdarg.h>
+#include <sys/param.h>
+
+#include "minidlnatypes.h"
+
+/* String functions */
+/* We really want this one inlined, since it has a major performance impact */
+static inline int strcatf(struct string_s *str, const char *fmt, ...)
+{
+	int ret;
+	int size;
+	va_list ap;
+
+	if (str->off >= str->size)
+		return 0;
+
+	va_start(ap, fmt);
+	size = str->size - str->off;
+	ret = vsnprintf(str->data + str->off, size, fmt, ap);
+	str->off += MIN(ret, size);
+	va_end(ap);
+
+	return ret;
+}
+static inline void strncpyt(char *dst, const char *src, size_t len)
+{
+	strncpy(dst, src, len);
+	dst[len-1] = '\0';
+}
+int xasprintf(char **strp, char *fmt, ...);
+int ends_with(const char * haystack, const char * needle);
+char *trim(char *str);
+char *strstrc(const char *s, const char *p, const char t);
+char *strcasestrc(const char *s, const char *p, const char t);
+char *modifyString(char * string, const char * before, const char * after);
+char *escape_tag(const char *tag, int force_alloc);
+char *unescape_tag(const char *tag, int force_alloc);
+void strip_ext(char * name);
+
+/* Metadata functions */
+int is_video(const char * file);
+int is_audio(const char * file);
+int is_image(const char * file);
+int is_playlist(const char * file);
+int is_album_art(const char * name);
+int resolve_unknown_type(const char * path, media_types dir_type);
+const char *mime_to_ext(const char * mime);
+
+/* Others */
+int make_dir(char * path, mode_t mode);
+unsigned int DJBHash(const char *str, int len);
+
+#endif

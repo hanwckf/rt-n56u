c306dbc6ad96dea28f05798e03d9faa32d037394
andy.padavan <andy.padavan@gmail.com>
2014-04-20 02:14:36 +0800

    wifi drivers: fix BSS_Coexistence (when 80211N_DRAFT3 is enabled)

--- trunk/proprietary/rt_wifi/rtsoc/2.7.X.X/rt2860v2/ap/ap.c
+++ trunk/proprietary/rt_wifi/rtsoc/2.7.X.X/rt2860v2/ap/ap.c
@@ -2504,6 +2504,9 @@ VOID APOverlappingBSSScan(
 		pAd->CommonCfg.AddHTInfo.AddHtInfo.ExtChanOffset = 0;
 		pAd->CommonCfg.LastBSSCoexist2040.field.BSS20WidthReq = 1;
 		pAd->CommonCfg.Bss2040CoexistFlag |= BSS_2040_COEXIST_INFO_SYNC;
+		/* 2012/10/11 MTK patch while AP fall back to 20M, base band register didn't fall back to 20M */
+		pAd->CommonCfg.Bss2040NeedFallBack = 1;
+		pAd->CommonCfg.RegTransmitSetting.field.EXTCHA = 0;
 	}
 
 	return;	

be32d866e83739ae53f8ed43cf436e2b3621ac33
andy.padavan <andy.padavan@gmail.com>
2014-05-10 23:01:13 +0800

    kernel-3.0.x: ext4: fix trim length underflow with small trim length, commit 22f10457432387615fa1ae6e0375d9cacc50819b upstream

--- trunk/linux-3.0.x/fs/ext4/mballoc.c
+++ trunk/linux-3.0.x/fs/ext4/mballoc.c
@@ -4910,6 +4910,8 @@ int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
 
 	if (unlikely(minlen > EXT4_BLOCKS_PER_GROUP(sb)))
 		return -EINVAL;
+	if (start + len <= first_data_blk)
+		goto out;
 	if (start < first_data_blk) {
 		len -= first_data_blk - start;
 		start = first_data_blk;
@@ -4958,5 +4960,6 @@ int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
 	}
 	range->len = trimmed * sb->s_blocksize;
 
+out:
 	return ret;
 }

e6c949ba2a873dfa38583a8d5c5ca56a59f2ec03
andy.padavan <andy.padavan@gmail.com>
2014-05-18 02:14:40 +0800

    kernel-3.0.x: gro: fix truesize underestimation, commit de8261c2fa364397ed872fad1244d75364689168 upstream

--- trunk/linux-3.0.x/net/core/skbuff.c
+++ trunk/linux-3.0.x/net/core/skbuff.c
@@ -2846,7 +2846,7 @@ int skb_gro_receive(struct sk_buff **head, struct sk_buff *skb)
 	nskb->prev = p;
 
 	nskb->data_len += p->len;
-	nskb->truesize += p->len;
+	nskb->truesize += p->truesize;
 	nskb->len += p->len;
 
 	*head = nskb;
@@ -2856,6 +2856,7 @@ int skb_gro_receive(struct sk_buff **head, struct sk_buff *skb)
 	p = nskb;
 
 merge:
+	p->truesize += skb->truesize - len;
 	if (offset > headlen) {
 		unsigned int eat = offset - headlen;
 

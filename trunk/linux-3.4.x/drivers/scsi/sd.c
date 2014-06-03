f9ebefa20b553d95e88111551616b79bc0456c92
Vaughan Cao <vaughan.cao@oracle.com>
2014-06-03 17:37:30 +0800

    sd: notify block layer when using temporary change to cache_type
    This is a fix for commit 39c60a0948cc06139e2fbfe084f83cb7e7deae3b
    
      "sd: fix array cache flushing bug causing performance problems"
    
    We must notify the block layer via q->flush_flags after a temporary change
    of the cache_type to write through.  Without this, a SYNCHRONIZE CACHE
    command will still be generated.  This patch factors out a helper that
    can be called from sd_revalidate_disk and cache_type_store.
    
    Signed-off-by: Vaughan Cao <vaughan.cao@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- trunk/linux-3.4.x/drivers/scsi/sd.c
+++ trunk/linux-3.4.x/drivers/scsi/sd.c
@@ -129,6 +129,19 @@ static const char *sd_cache_types[] = {
 	"write back, no read (daft)"
 };
 
+static void sd_set_flush_flag(struct scsi_disk *sdkp)
+{
+	unsigned flush = 0;
+
+	if (sdkp->WCE) {
+		flush |= REQ_FLUSH;
+		if (sdkp->DPOFUA)
+			flush |= REQ_FUA;
+	}
+
+	blk_queue_flush(sdkp->disk->queue, flush);
+}
+
 static ssize_t
 sd_store_cache_type(struct device *dev, struct device_attribute *attr,
 		    const char *buf, size_t count)
@@ -172,6 +185,7 @@ sd_store_cache_type(struct device *dev, struct device_attribute *attr,
 	if (sdkp->cache_override) {
 		sdkp->WCE = wce;
 		sdkp->RCD = rcd;
+		sd_set_flush_flag(sdkp);
 		return count;
 	}
 
@@ -2460,7 +2474,6 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	struct scsi_disk *sdkp = scsi_disk(disk);
 	struct scsi_device *sdp = sdkp->device;
 	unsigned char *buffer;
-	unsigned flush = 0;
 
 	SCSI_LOG_HLQUEUE(3, sd_printk(KERN_INFO, sdkp,
 				      "sd_revalidate_disk\n"));
@@ -2505,13 +2518,7 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	 * We now have all cache related info, determine how we deal
 	 * with flush requests.
 	 */
-	if (sdkp->WCE) {
-		flush |= REQ_FLUSH;
-		if (sdkp->DPOFUA)
-			flush |= REQ_FUA;
-	}
-
-	blk_queue_flush(sdkp->disk->queue, flush);
+	sd_set_flush_flag(sdkp);
 
 	set_capacity(disk, sdkp->capacity);
 	kfree(buffer);

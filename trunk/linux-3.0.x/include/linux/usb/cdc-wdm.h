6ceb770e89618cb0411061f18a866faa7f4a75a9
andy.padavan <andy.padavan@gmail.com>
2014-06-06 19:22:22 +0800

    kernel-3.x.x: cdc-wdm: cumulative commits upstream

--- trunk/linux-3.0.x/include/linux/usb/cdc-wdm.h
+++ trunk/linux-3.0.x/include/linux/usb/cdc-wdm.h
@@ -11,6 +11,16 @@
 #ifndef __LINUX_USB_CDC_WDM_H
 #define __LINUX_USB_CDC_WDM_H
 
+/*
+ * This IOCTL is used to retrieve the wMaxCommand for the device,
+ * defining the message limit for both reading and writing.
+ *
+ * For CDC WDM functions this will be the wMaxCommand field of the
+ * Device Management Functional Descriptor.
+ */
+#define IOCTL_WDM_MAX_COMMAND _IOR('H', 0xA0, __u16)
+
+
 extern struct usb_driver *usb_cdc_wdm_register(struct usb_interface *intf,
 					struct usb_endpoint_descriptor *ep,
 					int bufsize,

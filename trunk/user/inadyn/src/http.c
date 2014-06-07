7dab1e566cd38df0f33bfbd3c3edb98648ced90f
andy.padavan <andy.padavan@gmail.com>
2014-06-07 17:28:15 +0800

    inadyn: delete bogus preprocessor directive, fixed broken http code w/o SSL build

--- trunk/user/inadyn/src/http.c
+++ trunk/user/inadyn/src/http.c
@@ -143,11 +143,9 @@ int http_transaction(http_t *client, http_trans_t *trans)
 	trans->rsp_len = 0;
 	do {
 		if (client->ssl_enabled) {
-#ifdef CONFIG_OPENSSL
 			TRY(ssl_send(client, trans->p_req, trans->req_len));
 			TRY(ssl_recv(client, trans->p_rsp, trans->max_rsp_len, &trans->rsp_len));
 		} else {
-#endif
 			TRY(tcp_send(&client->tcp, trans->p_req, trans->req_len));
 			TRY(tcp_recv(&client->tcp, trans->p_rsp, trans->max_rsp_len, &trans->rsp_len));
 		}

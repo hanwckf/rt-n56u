e4a9e5f4172733c638a365b71c79c85bb8246a70
andy.padavan <andy.padavan@gmail.com>
2014-06-18 06:06:48 +0800

    kernel-3.0.x: implement upstream patches from 3.4.93-94 updates

--- trunk/linux-3.0.x/net/ipv4/netfilter/nf_defrag_ipv4.c
+++ trunk/linux-3.0.x/net/ipv4/netfilter/nf_defrag_ipv4.c
@@ -21,7 +21,6 @@
 #include <net/netfilter/nf_conntrack.h>
 #endif
 
-/* Returns new sk_buff, or NULL */
 static int nf_ct_ipv4_gather_frags(struct sk_buff *skb, u_int32_t user)
 {
 	int err;
@@ -32,8 +31,10 @@ static int nf_ct_ipv4_gather_frags(struct sk_buff *skb, u_int32_t user)
 	err = ip_defrag(skb, user);
 	local_bh_enable();
 
-	if (!err)
+	if (!err) {
 		ip_send_check(ip_hdr(skb));
+		skb->local_df = 1;
+	}
 
 	return err;
 }

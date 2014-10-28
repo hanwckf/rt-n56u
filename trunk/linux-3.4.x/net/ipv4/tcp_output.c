4fa6dfd865dca1a0f4675969aed8fd985a263280
andy.padavan <andy.padavan@gmail.com>
2014-10-29 04:34:46 +0800

    kernel-3.x.x: tcp: tcp_transmit_skb() optimizations, commit ccdbb6 upstream

--- trunk/linux-3.4.x/net/ipv4/tcp_output.c
+++ trunk/linux-3.4.x/net/ipv4/tcp_output.c
@@ -810,13 +810,13 @@ static int tcp_transmit_skb(struct sock *sk, struct sk_buff *skb, int clone_it,
 
 	BUG_ON(!skb || !tcp_skb_pcount(skb));
 
-	/* If congestion control is doing timestamping, we must
-	 * take such a timestamp before we potentially clone/copy.
-	 */
-	if (icsk->icsk_ca_ops->flags & TCP_CONG_RTT_STAMP)
-		__net_timestamp(skb);
+	if (clone_it) {
+		/* If congestion control is doing timestamping, we must
+		 * take such a timestamp before we potentially clone/copy.
+		 */
+		if (icsk->icsk_ca_ops->flags & TCP_CONG_RTT_STAMP)
+			__net_timestamp(skb);
 
-	if (likely(clone_it)) {
 		if (unlikely(skb_cloned(skb)))
 			skb = pskb_copy(skb, gfp_mask);
 		else

820fd2e43213e7df00db30496e0885080ab01d7d
andy.padavan <andy.padavan@gmail.com>
2014-10-30 01:35:01 +0800

    kernel-3.x.x: ipv4: arp: update neighbour address when a gratuitous arp is received and arp_accept is set, commit 56022a upstream

--- trunk/linux-3.4.x/net/ipv4/arp.c
+++ trunk/linux-3.4.x/net/ipv4/arp.c
@@ -734,6 +734,7 @@ static int arp_process(struct sk_buff *skb)
 	int addr_type;
 	struct neighbour *n;
 	struct net *net = dev_net(dev);
+	bool is_garp = false;
 
 	/* arp_rcv below verifies the ARP header and verifies the device
 	 * is ARP'able.
@@ -893,10 +894,12 @@ static int arp_process(struct sk_buff *skb)
 		   It is possible, that this option should be enabled for some
 		   devices (strip is candidate)
 		 */
+		is_garp = arp->ar_op == htons(ARPOP_REQUEST) && tip == sip &&
+			  inet_addr_type(net, sip) == RTN_UNICAST;
+
 		if (n == NULL &&
-		    (arp->ar_op == htons(ARPOP_REPLY) ||
-		     (arp->ar_op == htons(ARPOP_REQUEST) && tip == sip)) &&
-		    inet_addr_type(net, sip) == RTN_UNICAST)
+		    ((arp->ar_op == htons(ARPOP_REPLY)  &&
+		      inet_addr_type(net, sip) == RTN_UNICAST) || is_garp))
 			n = __neigh_lookup(&arp_tbl, &sip, dev, 1);
 	}
 
@@ -909,7 +912,7 @@ static int arp_process(struct sk_buff *skb)
 		   agents are active. Taking the first reply prevents
 		   arp trashing and chooses the fastest router.
 		 */
-		override = time_after(jiffies, n->updated + n->parms->locktime);
+		override = time_after(jiffies, n->updated + n->parms->locktime) || is_garp;
 
 		/* Broadcast replies and request packets
 		   do not assert neighbour reachability.

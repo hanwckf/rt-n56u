3246bdc00158ddcbd711c9755604e74da041f1fc
andy.padavan <andy.padavan@gmail.com>
2014-10-29 22:00:19 +0800

    kernel-3.4.x: squashfs: revert bogus commit from OpenWRT tree

--- trunk/linux-3.4.x/fs/squashfs/xz_wrapper.c
+++ trunk/linux-3.4.x/fs/squashfs/xz_wrapper.c
@@ -39,10 +39,8 @@ struct squashfs_xz {
 };
 
 struct comp_opts {
-	__le32 flags;
-	__le16 bit_opts;
-	__le16 fb;
 	__le32 dictionary_size;
+	__le32 flags;
 };
 
 static void *squashfs_xz_init(struct squashfs_sb_info *msblk, void *buff,

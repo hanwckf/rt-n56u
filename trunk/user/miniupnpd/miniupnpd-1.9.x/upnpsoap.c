01d85f2f0a361b47b9b1769fcbe11cb3796bc57f
andy.padavan <andy.padavan@gmail.com>
2014-11-30 01:32:25 +0800

    miniupnpd: update to 1.9.20141128

--- trunk/user/miniupnpd/miniupnpd-1.9.x/upnpsoap.c
+++ trunk/user/miniupnpd/miniupnpd-1.9.x/upnpsoap.c
@@ -1,4 +1,4 @@
-/* $Id: upnpsoap.c,v 1.128 2014/09/25 09:02:25 nanard Exp $ */
+/* $Id: upnpsoap.c,v 1.130 2014/11/28 13:18:57 nanard Exp $ */
 /* MiniUPnP project
  * http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
  * (c) 2006-2014 Thomas Bernard
@@ -170,8 +170,7 @@ GetCommonLinkProperties(struct upnphttp * h, const char * action)
 	static const char resp[] =
 		"<u:%sResponse "
 		"xmlns:u=\"%s\">"
-		/*"<NewWANAccessType>DSL</NewWANAccessType>"*/
-		"<NewWANAccessType>Cable</NewWANAccessType>"
+		"<NewWANAccessType>%s</NewWANAccessType>"
 		"<NewLayer1UpstreamMaxBitRate>%lu</NewLayer1UpstreamMaxBitRate>"
 		"<NewLayer1DownstreamMaxBitRate>%lu</NewLayer1DownstreamMaxBitRate>"
 		"<NewPhysicalLinkStatus>%s</NewPhysicalLinkStatus>"
@@ -182,6 +181,7 @@ GetCommonLinkProperties(struct upnphttp * h, const char * action)
 	struct ifdata data;
 	const char * status = "Up";	/* Up, Down (Required),
 	                             * Initializing, Unavailable (Optional) */
+	const char * wan_access_type = "Cable"; /* DSL, POTS, Cable, Ethernet */
 	char ext_ip_addr[INET_ADDRSTRLEN];
 
 	if((downstream_bitrate == 0) || (upstream_bitrate == 0))
@@ -197,7 +197,8 @@ GetCommonLinkProperties(struct upnphttp * h, const char * action)
 	}
 	bodylen = snprintf(body, sizeof(body), resp,
 	    action, "urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1",
-		upstream_bitrate, downstream_bitrate,
+	    wan_access_type,
+	    upstream_bitrate, downstream_bitrate,
 	    status, action);
 	BuildSendAndCloseSoapResp(h, body, bodylen);
 }
@@ -2042,34 +2043,32 @@ ExecuteSoapAction(struct upnphttp * h, const char * action, int n)
 	char * p2;
 	int i, len, methodlen;
 
-	i = 0;
+	/* SoapAction example :
+	 * urn:schemas-upnp-org:service:WANIPConnection:1#GetStatusInfo */
 	p = strchr(action, '#');
-
-	if(p)
-	{
+	if(p && (p - action) < n) {
 		p++;
 		p2 = strchr(p, '"');
-		if(p2)
+		if(p2 && (p2 - action) <= n)
 			methodlen = p2 - p;
 		else
 			methodlen = n - (p - action);
-		/*syslog(LOG_DEBUG, "SoapMethod: %.*s", methodlen, p);*/
-		while(soapMethods[i].methodName)
-		{
+		/*syslog(LOG_DEBUG, "SoapMethod: %.*s %d %d %p %p %d",
+		       methodlen, p, methodlen, n, action, p, (int)(p - action));*/
+		for(i = 0; soapMethods[i].methodName; i++) {
 			len = strlen(soapMethods[i].methodName);
-			if((len == methodlen) && memcmp(p, soapMethods[i].methodName, len) == 0)
-			{
+			if((len == methodlen) && memcmp(p, soapMethods[i].methodName, len) == 0) {
 #ifdef DEBUG
 				syslog(LOG_DEBUG, "Remote Call of SoapMethod '%s'\n",
 				       soapMethods[i].methodName);
-#endif
+#endif /* DEBUG */
 				soapMethods[i].methodImpl(h, soapMethods[i].methodName);
 				return;
 			}
-			i++;
 		}
-
 		syslog(LOG_DEBUG, "SoapMethod: Unknown: %.*s", methodlen, p);
+	} else {
+		syslog(LOG_DEBUG, "cannot parse SoapAction");
 	}
 
 	SoapError(h, 401, "Invalid Action");

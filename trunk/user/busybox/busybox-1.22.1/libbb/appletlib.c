2b3aa04b7d3fa1d458a2cf8153ad6f24d7c515fb
andy.padavan <andy.padavan@gmail.com>
2014-12-14 20:05:07 +0800

    busybox: Fix Variable set but unused warning

--- trunk/user/busybox/busybox-1.22.1/libbb/appletlib.c
+++ trunk/user/busybox/busybox-1.22.1/libbb/appletlib.c
@@ -687,7 +687,8 @@ static int busybox_main(char **argv)
 		return 0;
 	}
 
-	if (ENABLE_FEATURE_INSTALLER && strcmp(argv[1], "--install") == 0) {
+# if ENABLE_FEATURE_INSTALLER
+	if (strcmp(argv[1], "--install") == 0) {
 		int use_symbolic_links;
 		const char *busybox;
 
@@ -710,6 +711,7 @@ static int busybox_main(char **argv)
 		install_links(busybox, use_symbolic_links, argv[2]);
 		return 0;
 	}
+# endif
 
 	if (strcmp(argv[1], "--help") == 0) {
 		/* "busybox --help [<applet>]" */

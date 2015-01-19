c9fa1b2d351c5a7d2b1d9952b292e97bc1c35237
andy.padavan <andy.padavan@gmail.com>
2015-01-19 20:16:13 +0700

    project tree: add support ASUS RT-N54U device

---
+++ trunk/configs/boards/RT-N54U/board.h
@@ -0,0 +1,27 @@
+/* ASUS RT-N54U */
+
+#define BOARD_PID		"RT-N54U"
+#define BOARD_NAME		"RT-N54U"
+#define BOARD_DESC		"ASUS RT-N54U Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN54U/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	1
+#define BOARD_GPIO_BTN_WPS	2
+#undef  BOARD_GPIO_BTN_WLTOG
+#define BOARD_GPIO_LED_ALL	10
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	9
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	14
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

89d455fb97391bbb75a7ddbb494e14928f477a43
andy.padavan <andy.padavan@gmail.com>
2015-02-17 04:46:46 +0700

    project configs: add ASUS RT-N56U B1 target profile (MT7621)

---
+++ trunk/configs/boards/RT-N56UB1/board.h
@@ -0,0 +1,30 @@
+/* ASUS RT-N56U B1 */
+
+#define BOARD_PID		"RT-N56UB1"
+#define BOARD_NAME		"RT-N56UB1"
+#define BOARD_DESC		"ASUS RT-N56U B1 Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN56UB1/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	6
+#define BOARD_GPIO_BTN_WPS	18
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	14	/* 14: 2.4G, 15: 5G */
+#define BOARD_GPIO_LED_POWER	12
+#define BOARD_GPIO_LED_LAN	7
+#define BOARD_GPIO_LED_WAN	16
+#define BOARD_GPIO_LED_USB	13
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	1
+

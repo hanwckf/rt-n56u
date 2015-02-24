2d49654a5d9d20b78bedbad608691fb96c9783a4
andy.padavan <andy.padavan@gmail.com>
2015-02-24 16:54:16 +0700

    kernel-3.4.x: MIPS: Add option to disable software I/O coherency, commits b6d92b4a, 885014bc upstream

--- trunk/linux-3.4.x/arch/mips/include/asm/mach-generic/dma-coherence.h
+++ trunk/linux-3.4.x/arch/mips/include/asm/mach-generic/dma-coherence.h
@@ -59,12 +59,7 @@ static inline int plat_dma_mapping_error(struct device *dev,
 
 static inline int plat_device_is_coherent(struct device *dev)
 {
-#ifdef CONFIG_DMA_COHERENT
-	return 1;
-#endif
-#ifdef CONFIG_DMA_NONCOHERENT
-	return 0;
-#endif
+	return coherentio;
 }
 
 #endif /* __ASM_MACH_GENERIC_DMA_COHERENCE_H */

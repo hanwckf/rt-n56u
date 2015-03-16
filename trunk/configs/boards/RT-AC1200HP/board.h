7b3603db5f1c37f71ed519513029f6e7cf154d63
andy.padavan <andy.padavan@gmail.com>
2015-03-17 01:47:15 +0700

    project configs: add ASUS RT-AC1200HP profile

---
+++ trunk/configs/boards/RT-AC1200HP/board.h
@@ -0,0 +1,29 @@
+/* ASUS RT-AC1200HP */
+
+#define BOARD_PID		"RT-AC1200HP"
+#define BOARD_NAME		"RT-AC1200HP"
+#define BOARD_DESC		"ASUS RT-AC1200HP Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTAC1200HP/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	62
+#define BOARD_GPIO_BTN_WPS	61
+#define BOARD_GPIO_BTN_WLTOG	66
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define BOARD_GPIO_LED_SW5G	70	/* soft led */
+#define BOARD_GPIO_LED_POWER	65
+#define BOARD_GPIO_LED_LAN	69
+#undef  BOARD_GPIO_LED_WAN	68
+#define BOARD_GPIO_LED_USB	67
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	1
+

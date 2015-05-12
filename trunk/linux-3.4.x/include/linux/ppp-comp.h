72c160197152f91981e2f27e2a9b12a4e5b6fd4e
andy.padavan <andy.padavan@gmail.com>
2015-05-12 22:46:11 +0700

    kernel-3.x.x: revert ppp_mppe: Check coherency counter for out-of-order sequencing

--- trunk/linux-3.4.x/include/linux/ppp-comp.h
+++ trunk/linux-3.4.x/include/linux/ppp-comp.h
@@ -86,9 +86,8 @@ struct compressor {
 /*
  * The return value from decompress routine is the length of the
  * decompressed packet if successful, otherwise DECOMP_ERROR
- * or DECOMP_FATALERROR if an error occurred but don't want the
- * PPP generic layer to drop the packet.
- *
+ * or DECOMP_FATALERROR if an error occurred.
+ * 
  * We need to make this distinction so that we can disable certain
  * useful functionality, namely sending a CCP reset-request as a result
  * of an error detected after decompression.  This is to avoid infringing
@@ -98,7 +97,6 @@ struct compressor {
 
 #define DECOMP_ERROR		-1	/* error detected before decomp. */
 #define DECOMP_FATALERROR	-2	/* error detected after decomp. */
-#define DECOMP_DROPERROR	-3	/* error detected, drop packet. */
 
 /*
  * CCP codes.

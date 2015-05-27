f18649eed85dc09021ef021cf5e18d880de917b6
andy.padavan <andy.padavan@gmail.com>
2015-05-27 21:07:51 +0700

    scripts: increase ipv6 route max_size, remove force enable arp_filter

--- trunk/user/scripts/reset_ss.sh
+++ trunk/user/scripts/reset_ss.sh
@@ -22,7 +22,6 @@ echo 64       > /proc/sys/net/unix/max_dgram_qlen
 echo 30       > /proc/sys/net/ipv4/igmp_max_memberships
 
 # arp
-echo 1        > /proc/sys/net/ipv4/conf/all/arp_filter
 echo 1        > /proc/sys/net/ipv4/conf/all/arp_announce
 echo 1        > /proc/sys/net/ipv4/conf/default/arp_announce
 
@@ -31,11 +30,12 @@ echo 256      > /proc/sys/net/ipv4/neigh/default/gc_thresh1
 echo 1024     > /proc/sys/net/ipv4/neigh/default/gc_thresh2
 echo 2048     > /proc/sys/net/ipv4/neigh/default/gc_thresh3
 
-# neigh ipv6
+# ipv6
 if [ -d /proc/sys/net/ipv6 ] ; then
-	echo 256      > /proc/sys/net/ipv6/neigh/default/gc_thresh1
-	echo 1024     > /proc/sys/net/ipv6/neigh/default/gc_thresh2
-	echo 2048     > /proc/sys/net/ipv6/neigh/default/gc_thresh3
+  echo 256    > /proc/sys/net/ipv6/neigh/default/gc_thresh1
+  echo 1024   > /proc/sys/net/ipv6/neigh/default/gc_thresh2
+  echo 2048   > /proc/sys/net/ipv6/neigh/default/gc_thresh3
+  echo 16384  > /proc/sys/net/ipv6/route/max_size
 fi
 
 # reverse-path filter

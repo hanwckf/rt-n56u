863c2be054c5f8727a7613bf38b732e1d3aa10fa
andy.padavan <andy.padavan@gmail.com>
2015-08-07 04:43:41 +0700

    kernel-3.x.x: hci: Return correct number of tranferred bytes for stalled control endpoints, commit 22ae47e6 upstream

--- trunk/linux-3.0.x/drivers/usb/host/xhci-ring.c
+++ trunk/linux-3.0.x/drivers/usb/host/xhci-ring.c
@@ -1991,7 +1991,7 @@ static int process_ctrl_td(struct xhci_hcd *xhci, struct xhci_td *td,
 			td->urb->actual_length =
 				td->urb->transfer_buffer_length -
 				EVENT_TRB_LEN(le32_to_cpu(event->transfer_len));
-		else
+		else if (!td->urb_length_set)
 			td->urb->actual_length = 0;
 
 		xhci_cleanup_halted_endpoint(xhci,

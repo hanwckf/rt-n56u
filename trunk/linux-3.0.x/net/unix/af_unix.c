09f148cadc691d58048db5bf7cdcf8049ddc587c
andy.padavan <andy.padavan@gmail.com>
2015-10-09 00:45:30 +0700

    kernel-3.x.x: unix/caif: sk_socket can disappear when state is unlocked
    [ Upstream b48732e4a4 ]

--- trunk/linux-3.0.x/net/unix/af_unix.c
+++ trunk/linux-3.0.x/net/unix/af_unix.c
@@ -1874,6 +1874,10 @@ static long unix_stream_data_wait(struct sock *sk, long timeo)
 		unix_state_unlock(sk);
 		timeo = schedule_timeout(timeo);
 		unix_state_lock(sk);
+
+		if (sock_flag(sk, SOCK_DEAD))
+			break;
+
 		clear_bit(SOCK_ASYNC_WAITDATA, &sk->sk_socket->flags);
 	}
 
@@ -1934,6 +1938,10 @@ static int unix_stream_recvmsg(struct kiocb *iocb, struct socket *sock,
 		struct sk_buff *skb;
 
 		unix_state_lock(sk);
+		if (sock_flag(sk, SOCK_DEAD)) {
+			err = -ECONNRESET;
+			goto unlock;
+		}
 		skb = skb_peek(&sk->sk_receive_queue);
 		if (skb == NULL) {
 			unix_sk(sk)->recursion_level = 0;

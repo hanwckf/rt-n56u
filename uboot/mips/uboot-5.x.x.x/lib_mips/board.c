6a0ebd2faaa5129f9b5c9d63d4ed4a3571ded982
andy.padavan <andy.padavan@gmail.com>
2015-10-19 02:20:37 +0700

    U-Boot: allow recovery erasing separated NVRAM partition (for NAND only)

--- uboot/mips/uboot-5.x.x.x/lib_mips/board.c
+++ uboot/mips/uboot-5.x.x.x/lib_mips/board.c
@@ -3364,14 +3364,23 @@ int reset_to_default(void)
 
 	addr = CFG_ENV_ADDR;
 	size = CFG_CONFIG_SIZE;
-	printf("Erase 0x%08x size 0x%x\n", addr, size);
 
-	/* Restore to default. */
+	/* Erase U-Boot Env partition */
 #if defined (CFG_ENV_IS_IN_NAND)
+	/* Erase only one NAND block */
+	size = CFG_BLOCKSIZE;
 	ranand_erase((addr-CFG_FLASH_BASE), size);
+
+	/* Erase 'Config' partition with NVRAM */
+	if (CFG_NVRAM_ADDR > addr) {
+		addr = CFG_NVRAM_ADDR;
+		ranand_erase((addr-CFG_FLASH_BASE), size);
+	}
 #elif defined (CFG_ENV_IS_IN_SPI)
+	printf("Erase 0x%08x size 0x%x\n", addr, size);
 	raspi_erase((addr-CFG_FLASH_BASE), size);
 #else
+	printf("Erase 0x%08x size 0x%x\n", addr, size);
 	flash_sect_protect(0, addr, addr+size-1);
 	flash_sect_erase(addr, addr+size-1);
 	flash_sect_protect(1, addr, addr+size-1);

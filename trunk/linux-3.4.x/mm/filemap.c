cfb650eb7db2c1480f6718f5c10b9bea4ce2283a
andy.padavan <andy.padavan@gmail.com>
2015-11-14 23:33:42 +0700

    kernel-3.4.x: mm: make sendfile(2) killable
    [ Upstream 296291cdd1 ]

--- trunk/linux-3.4.x/mm/filemap.c
+++ trunk/linux-3.4.x/mm/filemap.c
@@ -2473,6 +2473,11 @@ again:
 			break;
 		}
 
+		if (fatal_signal_pending(current)) {
+			status = -EINTR;
+			break;
+		}
+
 		status = a_ops->write_begin(file, mapping, pos, bytes, flags,
 						&page, &fsdata);
 		if (unlikely(status))
@@ -2513,10 +2518,6 @@ again:
 		written += copied;
 
 		balance_dirty_pages_ratelimited(mapping);
-		if (fatal_signal_pending(current)) {
-			status = -EINTR;
-			break;
-		}
 	} while (iov_iter_count(i));
 
 	return written ? written : status;

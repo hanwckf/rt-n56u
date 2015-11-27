1f62770687459d6a1b3b6a8077fea7648e650253
andy.padavan <andy.padavan@gmail.com>
2015-11-27 21:58:25 +0700

    dnsmasq: Fix crash when empty address from DNS overlays A record from hosts

--- trunk/user/dnsmasq/dnsmasq-2.7x/src/cache.c
+++ trunk/user/dnsmasq/dnsmasq-2.7x/src/cache.c
@@ -481,7 +481,7 @@ struct crec *cache_insert(char *name, struct all_addr *addr,
 	 existing record is for an A or AAAA and
 	 the record we're trying to insert is the same, 
 	 just drop the insert, but don't error the whole process. */
-      if ((flags & (F_IPV4 | F_IPV6)) && (flags & F_FORWARD))
+      if ((flags & (F_IPV4 | F_IPV6)) && (flags & F_FORWARD) && addr)
 	{
 	  if ((flags & F_IPV4) && (new->flags & F_IPV4) &&
 	      new->addr.addr.addr.addr4.s_addr == addr->addr.addr4.s_addr)

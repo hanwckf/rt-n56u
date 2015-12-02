5f6111cb4624869fcfa40f16d1ac305c5ae781f1
andy.padavan <andy.padavan@gmail.com>
2015-12-03 01:03:06 +0700

    kernel-3.x.x: usbnet: add support new devices
    [ Upstream 227b9e8708, 0db65fcfcd, 7091079173, 0b88393cdf, 68242a5a1e ]

--- trunk/linux-3.4.x/drivers/net/usb/smsc75xx.c
+++ trunk/linux-3.4.x/drivers/net/usb/smsc75xx.c
@@ -1171,11 +1171,6 @@ static int smsc75xx_rx_fixup(struct usbnet *dev, struct sk_buff *skb)
 			skb_pull(skb, align_count);
 	}
 
-	if (unlikely(skb->len < 0)) {
-		netdev_warn(dev->net, "invalid rx length<0 %d", skb->len);
-		return 0;
-	}
-
 	return 1;
 }
 

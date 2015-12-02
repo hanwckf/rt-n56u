46b0c2ed7f01ae5c1d9d691f408ae104afd80375
andy.padavan <andy.padavan@gmail.com>
2015-12-02 16:26:19 +0700

    ralink arch: i2c busses: add support MT7621 and MT7628

--- trunk/linux-3.0.x/drivers/i2c/busses/i2c-ralink.c
+++ trunk/linux-3.0.x/drivers/i2c/busses/i2c-ralink.c
@@ -9,86 +9,145 @@
 #include <linux/i2c.h>
 #include <linux/io.h>
 
-#include <asm/rt2880/rt_mmap.h>
+#include <ralink/ralink_gpio.h>
 #include "i2c-ralink.h"
+
+#define I2C_MAX_BUSY_US		200000
+#define I2C_MAX_DONE_US		500000
 
 static int i2c_id;
 
-static inline unsigned char 
-construct_addr(struct i2c_msg *msg) 
-{
-	unsigned char addr;
-
-	addr = (msg->addr << 1);
-
-	if (msg->flags & I2C_M_RD)
-		addr |= 1;
-	return addr;   
-}
-
-static void 
+static void
 ralink_i2c_reset(void)
 {
 	u32 val;
 
-        /* reset i2c block */
-        val = RT2880_REG(RT2880_RSTCTRL_REG);
-	val = val | RALINK_I2C_RST;
-        RT2880_REG(RT2880_RSTCTRL_REG) = val;
+	/* reset i2c block */
+	val = RT2880_REG(RT2880_RSTCTRL_REG);
+	val |= RALINK_I2C_RST;
+	RT2880_REG(RT2880_RSTCTRL_REG) = val;
 
-	val = val & ~RALINK_I2C_RST;
-        RT2880_REG(RT2880_RSTCTRL_REG) = val;
-} 
+	udelay(1);
 
-static void 
-ralink_i2c_enable(struct i2c_msg *msg)
-{
-	RT2880_REG(RT2880_I2C_DEVADDR_REG) = msg->addr;
-        RT2880_REG(RT2880_I2C_CONFIG_REG) = I2C_CFG_DEFAULT;
-        RT2880_REG(RT2880_I2C_CLKDIV_REG) = CLKDIV_VALUE;
+	val &= ~RALINK_I2C_RST;
+	RT2880_REG(RT2880_RSTCTRL_REG) = val;
+
+	udelay(500);
 }
 
-static int 
+static void
+ralink_i2c_master_init(struct i2c_msg *msg)
+{
+	u32 reg;
+
+	RT2880_REG(RT2880_I2C_CONFIG_REG) = I2C_CFG_DEFAULT;
+
+#if defined (CONFIG_RALINK_MT7621) || defined (CONFIG_RALINK_MT7628)
+	reg  = (1 << 31);			// the output is pulled hight by SIF master 0
+	reg |= (1 << 28);			// allow triggered in VSYNC pulse
+	reg |= (CLKDIV_VALUE<<16);		// clk div
+	reg |= (1 << 6);			// output H when SIF master 0 is in WAIT state
+	reg |= (1 << 1);			// enable SIF master 0
+	RT2880_REG(RT2880_I2C_SM0CTL0) = reg;
+	RT2880_REG(RT2880_I2C_SM0_IS_AUTOMODE) = 1; // auto mode
+#else
+	reg = CLKDIV_VALUE;
+	RT2880_REG(RT2880_I2C_CLKDIV_REG) = reg;
+#endif
+
+	/*
+	 * Device Address :
+	 *
+	 * ATMEL 24C152 serial EEPROM
+	 *       1|0|1|0|0|A1|A2|R/W
+	 *      MSB              LSB
+	 *
+	 * ATMEL 24C01A/02/04/08A/16A
+	 *      MSB               LSB
+	 * 1K/2K 1|0|1|0|A2|A1|A0|R/W
+	 * 4K            A2 A1 P0
+	 * 8K            A2 P1 P0
+	 * 16K           P2 P1 P0
+	 *
+	 * so device address needs 7 bits
+	 * if device address is 0,
+	 * write 0xA0 >> 1 into DEVADDR(max 7-bits) REG
+	 */
+	RT2880_REG(RT2880_I2C_DEVADDR_REG) = msg->addr;
+
+	/*
+	 * Use Address Disabled Transfer Options
+	 * because it just support 8-bits, 
+	 * ATMEL eeprom needs two 8-bits address
+	 */
+	RT2880_REG(RT2880_I2C_ADDR_REG) = 0;
+}
+
+static int
 ralink_i2c_wait_tx_done(void)
 {
-	while(!(RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_SDOEMPTY));
-	return 0;
+	int timeout = I2C_MAX_DONE_US;
+
+	while(timeout > 0) {
+		if (RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_SDOEMPTY)
+			return 0;
+		timeout--;
+		udelay(1);
+	}
+
+	return -1;
 }
 
-static int 
+static int
 ralink_i2c_wait_rx_done(void)
 {
-	while(!(RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_DATARDY));
-	return 0;
+	int timeout = I2C_MAX_DONE_US;
+
+	while(timeout > 0) {
+		if (RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_DATARDY)
+			return 0;
+		timeout--;
+		udelay(1);
+	}
+
+	return -1;
 }
 
-static int 
+static int
 ralink_i2c_wait_idle(void)
 {
-	while(RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_BUSY);
-	return 0;
+	int timeout = I2C_MAX_BUSY_US;
+
+	while(timeout > 0) {
+		if (!(RT2880_REG(RT2880_I2C_STATUS_REG) & RALINK_I2C_BUSY))
+			return 0;
+		timeout--;
+		udelay(1);
+	}
+
+	return -1;
 }
 
-static int 
-ralink_i2c_handle_msg(struct i2c_adapter *i2c_adap, struct i2c_msg* pmsg) 
+static int
+ralink_i2c_handle_msg(struct i2c_adapter *i2c_adap, struct i2c_msg* msg)
 {
 	int i=0, j=0, pos=0;
-	int nblock = pmsg->len / READ_BLOCK;
-        int rem = pmsg->len % READ_BLOCK;
+	int nblock = msg->len / READ_BLOCK;
+	int rem = msg->len % READ_BLOCK;
 
-	if (pmsg->flags & I2C_M_TEN) {
-		printk("10 bits addr not supported\n");
+	if (msg->flags & I2C_M_TEN) {
+		printk("%s: 10 bits addr not supported\n", "Ralink-I2C");
 		return -EINVAL;
 	}
 
-	if ((pmsg->flags&I2C_M_RD)) {
+	if ((msg->flags&I2C_M_RD)) {
 		for(i=0; i< nblock; i++) {
 			ralink_i2c_wait_idle();
 			RT2880_REG(RT2880_I2C_BYTECNT_REG) = READ_BLOCK-1;
 			RT2880_REG(RT2880_I2C_STARTXFR_REG) = READ_CMD;
 			for(j=0; j< READ_BLOCK; j++) {
 				ralink_i2c_wait_rx_done();
-				pmsg->buf[pos++] = RT2880_REG(RT2880_I2C_DATAIN_REG);
+				msg->buf[pos++] = RT2880_REG(RT2880_I2C_DATAIN_REG);
 			}
 		}
 
@@ -97,18 +156,18 @@ ralink_i2c_handle_msg(struct i2c_adapter *i2c_adap, struct i2c_msg* pmsg)
 		RT2880_REG(RT2880_I2C_STARTXFR_REG) = READ_CMD;
 		for(i=0; i< rem; i++) {
 			ralink_i2c_wait_rx_done();
-			pmsg->buf[pos++] = RT2880_REG(RT2880_I2C_DATAIN_REG);
+			msg->buf[pos++] = RT2880_REG(RT2880_I2C_DATAIN_REG);
 		}
 	} else {
 		ralink_i2c_wait_idle();
-		RT2880_REG(RT2880_I2C_BYTECNT_REG) = pmsg->len-1;
+		RT2880_REG(RT2880_I2C_BYTECNT_REG) = rem-1;
 
-		for(i=0; i< pmsg->len; i++) {
-			RT2880_REG(RT2880_I2C_DATAOUT_REG) = pmsg->buf[i];
+		for(i=0; i< rem; i++) {
+			RT2880_REG(RT2880_I2C_DATAOUT_REG) = msg->buf[i];
 
-			if(i==0) {
+			if (i==0)
 				RT2880_REG(RT2880_I2C_STARTXFR_REG) = WRITE_CMD;
-			}
+
 			ralink_i2c_wait_tx_done();
 		}
 	}
@@ -119,7 +178,7 @@ ralink_i2c_handle_msg(struct i2c_adapter *i2c_adap, struct i2c_msg* pmsg)
 /*
  * master_xfer() - main read/write entry
  */
-static int 
+static int
 ralink_i2c_master_xfer(struct i2c_adapter *i2c_adap, struct i2c_msg *msgs, 
 				int num)
 {
@@ -128,19 +187,19 @@ ralink_i2c_master_xfer(struct i2c_adapter *i2c_adap, struct i2c_msg *msgs,
 
 	ralink_i2c_wait_idle();
 	ralink_i2c_reset();
-	ralink_i2c_enable(msgs);
+	ralink_i2c_master_init(msgs);
 
 	for (im = 0; ret == 0 && im != num; im++) {
 		ret = ralink_i2c_handle_msg(i2c_adap, &msgs[im]);
 	}
 
-	if(ret)
+	if (ret)
 		return ret;
 
-	return im;   
+	return im;
 }
 
-static u32 
+static u32
 ralink_i2c_func(struct i2c_adapter *adap)
 {
 	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
@@ -148,10 +207,11 @@ ralink_i2c_func(struct i2c_adapter *adap)
 
 static const struct i2c_algorithm ralink_i2c_algo = {
 	.master_xfer	= ralink_i2c_master_xfer,
+	.smbus_xfer	= NULL,
 	.functionality	= ralink_i2c_func,
 };
 
-static int 
+static int
 ralink_i2c_remove(struct platform_device *pdev)
 {
 	struct i2c_adapter *padapter = platform_get_drvdata(pdev);
@@ -167,14 +227,13 @@ ralink_i2c_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int 
+static int
 ralink_i2c_probe(struct platform_device *pdev)
 {
 	struct resource *res;
 	int ret;
 	struct i2c_adapter *new_adapter;
 	struct i2c_algo_ralink_data *adapter_data;
-
 
 	new_adapter = kzalloc(sizeof(struct i2c_adapter), GFP_KERNEL);
 	if (!new_adapter) {
@@ -199,6 +258,9 @@ ralink_i2c_probe(struct platform_device *pdev)
 		goto free_both;
 	}
 
+	/* configure i2c to normal mode */
+	RT2880_REG(RALINK_SYSCTL_BASE + 0x60) &= ~RALINK_GPIOMODE_I2C;
+
 	adapter_data->id = i2c_id++;
 
 	memcpy(new_adapter->name, pdev->name, strlen(pdev->name));
@@ -219,7 +281,6 @@ ralink_i2c_probe(struct platform_device *pdev)
 	i2c_add_numbered_adapter(new_adapter);
 
 	return 0;
-
 
 free_both:
 	kfree(adapter_data);
@@ -242,13 +303,11 @@ static struct platform_driver ralink_i2c_driver = {
 
 static int __init i2c_ralink_init(void)
 {
-	printk("Ralink I2C Init\n");
 	return platform_driver_register(&ralink_i2c_driver);
 }
 
 static void __exit i2c_ralink_exit(void)
 {
-	printk("Ralink I2C Exit\n");
 	platform_driver_unregister(&ralink_i2c_driver);
 }
 
@@ -256,6 +315,6 @@ module_init(i2c_ralink_init);
 module_exit(i2c_ralink_exit);
 
 MODULE_AUTHOR("Steven Liu <steven_liu@mediatek.com>");
-MODULE_DESCRIPTION("Ralink I2c host driver");
+MODULE_DESCRIPTION("Ralink/MTK I2C host driver");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS("platform:Ralink-I2C");

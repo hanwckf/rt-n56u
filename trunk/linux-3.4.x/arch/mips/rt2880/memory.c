3d60a1b5fc5ef09fd8eccfa557805e5e2da4ee0c
andy.padavan <andy.padavan@gmail.com>
2015-12-19 23:41:42 +0700

    ralink arch: add CONFIG_LONG_CALLS independent from CONFIG_HIGHMEM

--- trunk/linux-3.4.x/arch/mips/rt2880/memory.c
+++ trunk/linux-3.4.x/arch/mips/rt2880/memory.c
@@ -98,8 +98,10 @@ void __init prom_meminit(void)
 		/* 1. Normal 0..448MB */
 		add_memory_region(RAM_BASE, 0x1c000000, BOOT_MEM_RAM);
 		
+#ifdef CONFIG_HIGHMEM
 		/* 2. Highmem (reserved yet, need highmem patch from MTI) */
 //		add_memory_region(0x20000000, (ramsize - 0x1c000000), BOOT_MEM_RAM);
+#endif
 	} else
 #endif
 	add_memory_region(RAM_BASE, ramsize, BOOT_MEM_RAM);

0195d9d15a55d3ff4b795a6f2eaa71bb13d28242
andy.padavan <andy.padavan@gmail.com>
2015-12-20 03:25:29 +0700

    kernel-3.x.x: net: qmi_wwan: ignore bogus CDC Union descriptors
    [ Upstream 34a55d5e85 ]

--- trunk/linux-3.0.x/drivers/net/usb/qmi_wwan.c
+++ trunk/linux-3.0.x/drivers/net/usb/qmi_wwan.c
@@ -429,7 +429,10 @@ next_desc:
 				"bogus CDC Union: master=%u, slave=%u\n",
 				cdc_union->bMasterInterface0,
 				cdc_union->bSlaveInterface0);
-			goto err;
+
+			/* ignore and continue... */
+			cdc_union = NULL;
+			info->data = intf;
 		}
 	}
 

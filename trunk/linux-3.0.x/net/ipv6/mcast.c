7a964842d98dcfb29e55707e2ceb86b30de38aca
andy.padavan <andy.padavan@gmail.com>
2015-12-29 03:55:34 +0700

    kernel-3.x.x: snmp: Remove duplicate OUTMCAST stat increment
    [ Upstream 41033f029e ]

--- trunk/linux-3.0.x/net/ipv6/mcast.c
+++ trunk/linux-3.0.x/net/ipv6/mcast.c
@@ -1442,7 +1442,6 @@ out:
 	if (!err) {
 		ICMP6MSGOUT_INC_STATS_BH(net, idev, ICMPV6_MLD2_REPORT);
 		ICMP6_INC_STATS_BH(net, idev, ICMP6_MIB_OUTMSGS);
-		IP6_UPD_PO_STATS_BH(net, idev, IPSTATS_MIB_OUTMCAST, payload_len);
 	} else
 		IP6_INC_STATS_BH(net, idev, IPSTATS_MIB_OUTDISCARDS);
 
@@ -1811,7 +1810,6 @@ out:
 	if (!err) {
 		ICMP6MSGOUT_INC_STATS(net, idev, type);
 		ICMP6_INC_STATS(net, idev, ICMP6_MIB_OUTMSGS);
-		IP6_UPD_PO_STATS(net, idev, IPSTATS_MIB_OUTMCAST, full_len);
 	} else
 		IP6_INC_STATS(net, idev, IPSTATS_MIB_OUTDISCARDS);
 

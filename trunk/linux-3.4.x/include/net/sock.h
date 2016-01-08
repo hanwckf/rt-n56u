ffacd3e87dea61fdf0e1a1cb886ae6f3d40a2c72
andy.padavan <andy.padavan@gmail.com>
2016-01-08 10:20:31 +0700

    kernel-3.x.x: net: fix warnings in 'make htmldocs' by moving macro definition out of field declaration
    [ Upstream 7bbadd2d10 ]

--- trunk/linux-3.4.x/include/net/sock.h
+++ trunk/linux-3.4.x/include/net/sock.h
@@ -322,8 +322,8 @@ struct sock {
 				sk_no_check  : 2,
 				sk_userlocks : 4,
 				sk_protocol  : 8,
-#define SK_PROTOCOL_MAX U8_MAX
 				sk_type      : 16;
+#define SK_PROTOCOL_MAX ((u8)~0U)
 	kmemcheck_bitfield_end(flags);
 	int			sk_wmem_queued;
 	gfp_t			sk_allocation;

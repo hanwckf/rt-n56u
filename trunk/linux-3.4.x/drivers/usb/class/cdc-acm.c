1b5f16040388a29b7758f88f8364a19a52678267
andy.padavan <andy.padavan@gmail.com>
2016-01-27 20:29:20 +0700

    kernel-3.x.x: USB: cdc_acm: Ignore Infineon Flash Loader utility, exclude Samsung phone 04e8:685d
    [ Upstream 1614265526, f33a7f72e5, e912e685f3 ]

--- trunk/linux-3.4.x/drivers/usb/class/cdc-acm.c
+++ trunk/linux-3.4.x/drivers/usb/class/cdc-acm.c
@@ -1013,6 +1013,10 @@ static int acm_probe(struct usb_interface *intf,
 
 	/* normal quirks */
 	quirks = (unsigned long)id->driver_info;
+
+	if (quirks == IGNORE_DEVICE)
+		return -ENODEV;
+
 	num_rx_buf = (quirks == SINGLE_RX_URB) ? 1 : ACM_NR;
 
 	/* handle quirks deadly to normal probing*/
@@ -1744,6 +1748,16 @@ static const struct usb_device_id acm_ids[] = {
 	/* Support for Droids MuIn LCD */
 	{ USB_DEVICE(0x04d8, 0x000b),
 	.driver_info = NO_DATA_INTERFACE,
+	},
+
+	/*Samsung phone in firmware update mode */
+	{ USB_DEVICE(0x04e8, 0x685d),
+	.driver_info = IGNORE_DEVICE,
+	},
+
+	/* Exclude Infineon Flash Loader utility */
+	{ USB_DEVICE(0x058b, 0x0041),
+	.driver_info = IGNORE_DEVICE,
 	},
 
 	/* control interfaces without any protocol set */

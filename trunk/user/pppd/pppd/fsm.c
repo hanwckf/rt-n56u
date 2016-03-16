8e2ff52ab21f8c949c89f55666e34ab6f72513ef
YASUOKA Masahiko <yasuoka@yasuoka.net>
2016-03-16 13:39:19 +0900

    pppd: Fix a typo in comment. Diff from Yuuichi Someya.

--- trunk/user/pppd/pppd/fsm.c
+++ trunk/user/pppd/pppd/fsm.c
@@ -467,7 +467,7 @@ fsm_rconfreq(f, id, inp, len)
 	f->nakloops = 0;
 
     } else {
-	/* we sent CONFACK or CONFREJ */
+	/* we sent CONFNAK or CONFREJ */
 	if (f->state != ACKRCVD)
 	    f->state = REQSENT;
 	if( code == CONFNAK )

5549acbd4976ebc556dea68e4d7f7ebc75192dd1
Florian Westphal <fw@strlen.de>
2016-03-22 18:02:49 +0100

    netfilter: x_tables: validate e->target_offset early
    commit bdf533de6968e9686df777dc178486f600c6e617 upstream.
    
    We should check that e->target_offset is sane before
    mark_source_chains gets called since it will fetch the target entry
    for loop detection.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    [bwh: Backported to 3.2: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- trunk/linux-3.4.x/net/ipv4/netfilter/ip_tables.c
+++ trunk/linux-3.4.x/net/ipv4/netfilter/ip_tables.c
@@ -325,7 +325,7 @@ struct ipt_entry *ipt_next_entry(const struct ipt_entry *entry)
 }
 
 #ifdef CONFIG_IP_NF_IPTABLES_SPEEDUP
-static bool
+static inline bool
 ipt_handle_default_rule(struct ipt_entry *e, unsigned int *verdict)
 {
 	struct xt_entry_target *t;
@@ -652,23 +652,13 @@ static void cleanup_match(struct xt_entry_match *m, struct net *net)
 	module_put(par.match->me);
 }
 
-static int
-#ifdef CONFIG_IP_NF_IPTABLES_SPEEDUP
-check_entry(struct ipt_entry *e, const char *name)
-#else
-check_entry(const struct ipt_entry *e, const char *name)
-#endif
+static inline int
+check_entry(const struct ipt_entry *e)
 {
 	const struct xt_entry_target *t;
 
-	if (!ip_checkentry(&e->ip)) {
-		duprintf("ip check failed %p %s.\n", e, name);
+	if (!ip_checkentry(&e->ip))
 		return -EINVAL;
-	}
-
-#ifdef CONFIG_IP_NF_IPTABLES_SPEEDUP
-	ip_checkdefault(&e->ip);
-#endif
 
 	if (e->target_offset + sizeof(struct xt_entry_target) >
 	    e->next_offset)
@@ -758,9 +748,9 @@ find_check_entry(struct ipt_entry *e, struct net *net, const char *name,
 	struct xt_mtchk_param mtpar;
 	struct xt_entry_match *ematch;
 
-	ret = check_entry(e, name);
-	if (ret)
-		return ret;
+#ifdef CONFIG_IP_NF_IPTABLES_SPEEDUP
+	ip_checkdefault(&e->ip);
+#endif
 
 	j = 0;
 	mtpar.net	= net;
@@ -825,6 +815,7 @@ check_entry_size_and_hooks(struct ipt_entry *e,
 			   unsigned int valid_hooks)
 {
 	unsigned int h;
+	int err;
 
 	if ((unsigned long)e % __alignof__(struct ipt_entry) != 0 ||
 	    (unsigned char *)e + sizeof(struct ipt_entry) >= limit) {
@@ -838,6 +829,10 @@ check_entry_size_and_hooks(struct ipt_entry *e,
 			 e, e->next_offset);
 		return -EINVAL;
 	}
+
+	err = check_entry(e);
+	if (err)
+		return err;
 
 	/* Check hooks & underflows */
 	for (h = 0; h < NF_INET_NUMHOOKS; h++) {
@@ -1647,7 +1642,7 @@ check_compat_entry_size_and_hooks(struct compat_ipt_entry *e,
 	}
 
 	/* For purposes of check_entry casting the compat entry is fine */
-	ret = check_entry((struct ipt_entry *)e, name);
+	ret = check_entry((struct ipt_entry *)e);
 	if (ret)
 		return ret;
 

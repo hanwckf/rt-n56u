b833adebef12a093d6541fe7df42ae9203d59f9a
andy.padavan <andy.padavan@gmail.com>
2016-03-23 23:45:34 +0700

    kernel-3.4.x: netfilter: nf_defrag_ipv4: Drop redundant ip_send_check()
    [ Upstream 5f547391f5 ]

--- trunk/linux-3.4.x/net/ipv4/netfilter/nf_defrag_ipv4.c
+++ trunk/linux-3.4.x/net/ipv4/netfilter/nf_defrag_ipv4.c
@@ -31,10 +31,8 @@ static int nf_ct_ipv4_gather_frags(struct sk_buff *skb, u_int32_t user)
 	err = ip_defrag(skb, user);
 	local_bh_enable();
 
-	if (!err) {
-		ip_send_check(ip_hdr(skb));
+	if (!err)
 		skb->local_df = 1;
-	}
 
 	return err;
 }

952e9d6cbf1c6d3fcc04511e0e92925638b52bf9
andy.padavan <andy.padavan@gmail.com>
2016-04-24 16:08:32 +0700

    kernel-3.x.x: net: add scheduling point in recvmmsg/sendmmsg
    [ Upstream a78cb84c62 ]

--- trunk/linux-3.0.x/net/socket.c
+++ trunk/linux-3.0.x/net/socket.c
@@ -2091,6 +2091,7 @@ int __sys_sendmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 		if (err)
 			break;
 		++datagrams;
+		cond_resched();
 	}
 
 	fput_light(sock->file, fput_needed);
@@ -2316,6 +2317,7 @@ int __sys_recvmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 		/* Out of band data, return right away */
 		if (msg_sys.msg_flags & MSG_OOB)
 			break;
+		cond_resched();
 	}
 
 out_put:
--- trunk/linux-3.4.x/net/socket.c
+++ trunk/linux-3.4.x/net/socket.c
@@ -2114,6 +2114,7 @@ int __sys_sendmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 		if (err)
 			break;
 		++datagrams;
+		cond_resched();
 	}
 
 	fput_light(sock->file, fput_needed);
@@ -2333,6 +2334,7 @@ int __sys_recvmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 		/* Out of band data, return right away */
 		if (msg_sys.msg_flags & MSG_OOB)
 			break;
+		cond_resched();
 	}
 
 out_put:

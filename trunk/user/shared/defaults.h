a3b84facbdb7a11725d56988a75acd4545d5f7a2
andy.padavan <andy.padavan@gmail.com>
2016-10-17 03:40:03 +0700

    shared: move default LAN IP values to defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -28,6 +28,11 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
+#define DEF_LAN_ADDR		"192.168.1.1"
+#define DEF_LAN_DHCP_BEG	"192.168.1.2"
+#define DEF_LAN_DHCP_END	"192.168.1.244"
+#define DEF_LAN_MASK		"255.255.255.0"
+
 #define DEF_WLAN_2G_CC		"GB"
 #define DEF_WLAN_5G_CC		"GB"
 #define DEF_WLAN_2G_SSID	"ASUS"

479a7c51845598250409acb27f50edfbd7e755c3
andy.padavan <andy.padavan@gmail.com>
2016-11-08 02:09:20 +0700

    raeth: fix init RTL8211F GPHY, allow read PHY EEE status

--- trunk/linux-3.4.x/drivers/net/raeth/ra_phy.h
+++ trunk/linux-3.4.x/drivers/net/raeth/ra_phy.h
@@ -2,6 +2,7 @@
 #define __RA_PHY_H__
 
 void ext_gphy_init(u32 phy_addr);
+u32  ext_gphy_fill_pmsr(u32 phy_addr);
 void ext_gphy_eee_enable(u32 phy_addr, int is_eee_enabled);
 void enable_autopoll_phy(int ge);
 void early_phy_init(void);

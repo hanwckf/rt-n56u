898b45766b893d9a28dc38544b010b7f8406cc05
andy.padavan <andy.padavan@gmail.com>
2016-11-28 00:11:33 +0700

    board profiles: add ZyXEL Keenetic 4G III rev.B profile

---
+++ trunk/configs/boards/KN-4G3B/board.h
@@ -0,0 +1,31 @@
+/* ZyXEL Keenetic 4G III rev.B (kg_rh) */
+
+#define BOARD_PID		"KN-4G3B"
+#define BOARD_NAME		"KN-4G3B"
+#define BOARD_DESC		"ZyXEL Keenetic 4G III rev.B Wireless Router"
+#define BOARD_VENDOR_NAME	"ZyXEL Communications Corp."
+#define BOARD_VENDOR_URL	"https://zyxel.ru/"
+#define BOARD_MODEL_URL		"https://zyxel.ru/keenetic-4g-3"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	5
+#define BOARD_GPIO_BTN_WPS	4
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	0	/* soft led */
+#define BOARD_GPIO_LED_POWER	38
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	2
+#define BOARD_GPIO_LED_USB	11
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
+#define BOARD_GPIO_PWR_USB	37	/* USB2.0 5V Power */
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	2
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

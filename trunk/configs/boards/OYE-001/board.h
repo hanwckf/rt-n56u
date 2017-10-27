bdb301fdd9f550f83edf8f5f0936353ce7da6143
hanwckf <my375229675@gmail.com>
2017-10-27 20:02:23 +0800

    适配PSG1218,OYE-001,中文汉化文件 汉化翻译来自于 https://gitee.com/gorden5566/padavan

---
+++ trunk/configs/boards/OYE-001/board.h
@@ -0,0 +1,28 @@
+/* OYE-001 */
+
+#define BOARD_PID		"OYE-001"
+#define BOARD_NAME		"OYE-001"
+#define BOARD_DESC		"OYE-001 Wireless Router"
+#define BOARD_VENDOR_NAME	"OYE"
+#define BOARD_VENDOR_URL	"http://www.oyewifi.com"
+#define BOARD_MODEL_URL		"http://www.oyewifi.com"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_RESET
+#define BOARD_GPIO_BTN_WPS	1
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define BOARD_GPIO_LED_POWER	9
+#define BOARD_GPIO_LED_LAN	43
+#define BOARD_GPIO_LED_WAN	44
+#define BOARD_GPIO_LED_USB	42
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

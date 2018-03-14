804a2e0120312173b6690b5940a62744af0fe1fa
hanwckf <my375229675@gmail.com>
2018-03-14 11:35:29 +0800

    disable ss udp relay disable mbedtls for ssr

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -508,7 +508,7 @@ struct nvram_pair router_defaults[] = {
 	{ "ss_server_port", "8989" },
 	{ "ss_key", "Secret" },
 	{ "ss_method", "rc4-md5" },
-	{ "ss_udp", "1" },
+	{ "ss_udp", "0" },
 	{ "ss_local_port", "1080" },
 	{ "ss_mtu", "1492" },
 	{ "ss_router_proxy", "1" },

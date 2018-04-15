59bd8e62c6c47b04a05c2d5d9b06430eda4e5539
df2016 <jcb20150301@gmail.com>
2018-04-15 21:56:00 +0800

    适配WR1200JS

---
+++ trunk/configs/boards/WR1200JS/board.h
@@ -0,0 +1,35 @@
+/*/* YOUHUA WR1200JS */
+
+#define BOARD_PID		"WR1200JS"
+#define BOARD_NAME		"WR1200JS"
+#define BOARD_DESC		"YOUHUA WR1200JS Wireless Router"
+#define BOARD_VENDOR_NAME	"Youhua Technology"
+#define BOARD_VENDOR_URL	"http://www.youhuatech.com/"
+#define BOARD_MODEL_URL		"http://www.youhuatech.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_FN1	18//WiFi
+#define BOARD_GPIO_BTN_RESET	3//RESET
+#define BOARD_GPIO_BTN_WPS	12//WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	6//Internet
+#define BOARD_GPIO_LED_USB	8//USB
+#define BOARD_GPIO_LED_ROUTER	7//WPS
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
+
+

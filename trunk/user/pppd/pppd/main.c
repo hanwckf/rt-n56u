ca39a57d762b116533f1705f917154c030129afb
Paul Mackerras <paulus@ozlabs.org>
2018-06-23 17:26:42 +1000

    pppd: Don't try to free(NULL - 1)
    A logic bug in update_script_environment() means that it can call
    remove_script_env() even when the variable being removed is not
    present in the script_env array.  The result of that is that
    remove_script_env() will call free() with argument NULL - 1.
    To fix this, we avoid calling remove_script_env() in this case.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- trunk/user/pppd/pppd/main.c
+++ trunk/user/pppd/pppd/main.c
@@ -1831,7 +1831,7 @@ update_script_environment()
 		script_env[i] = newstring;
 	    else
 		add_script_env(i, newstring);
-	} else {
+	} else if (p != NULL) {
 	    remove_script_env(i);
 	}
     }

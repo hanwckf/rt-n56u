6168912057749a545c7ce8c5081355334213a33b
hanwckf <my375229675@gmail.com>
2018-09-23 02:01:01 +0800

    adapted to MI-3(nand flash)

---
+++ trunk/configs/boards/MI-3/board.h
@@ -0,0 +1,30 @@
+/* Xiaomi Mi 3 */
+
+#define BOARD_PID		"MI-3"
+#define BOARD_NAME		"MI-3"
+#define BOARD_DESC		"Xiaomi Mi Mini Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifi3"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	30
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	24	/* 24: blue, 26: yellow, 29: red */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	26
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB	65
+#define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

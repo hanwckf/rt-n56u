bf788441051518786c3d6cb04846a13ec5e71730
hanwckf <my375229675@gmail.com>
2018-09-25 03:55:44 -0400

    fix scutclient_watchcat

--- trunk/user/scutclient/scripts/scutclient_watchcat.sh
+++ trunk/user/scutclient/scripts/scutclient_watchcat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ "$(pidof scutclient_watchcat.sh)" != "$$" ] && exit 1
-net="www.qq.com www.baidu.com www.taobao.com"
+net="114.114.114.114 119.29.29.29 202.38.193.188"
 net_num=3
 
 while true; do
@@ -23,5 +23,7 @@ while true; do
 			fails=$((fails+1))
 		fi
 	done
-	[ $fails -eq $net_num ] && logger -st "scutclient_watchcat" "Connection failed, restart scutclient!" && /bin/scutclient.sh restart
+	[ $fails -eq $net_num ] && \
+	logger -st "scutclient_watchcat" "Connection failed, restart scutclient!" && \
+	/bin/scutclient.sh restart
 done

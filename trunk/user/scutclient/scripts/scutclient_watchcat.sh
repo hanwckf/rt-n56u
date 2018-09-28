9d0065f4b2c368f65d8c92f000014b34021a9fba
hanwckf <my375229675@gmail.com>
2018-09-28 14:51:56 +0800

    update scutclient_watchcat

--- trunk/user/scutclient/scripts/scutclient_watchcat.sh
+++ trunk/user/scutclient/scripts/scutclient_watchcat.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 [ "$(pidof scutclient_watchcat.sh)" != "$$" ] && exit 1
-net="202.38.193.188 114.114.114.114 119.29.29.29"
-net_num=3
+net="202.38.193.188 114.114.114.114 119.29.29.29 202.38.193.33"
+net_num=4
 
 while true; do
 	sleep 60
@@ -17,11 +17,15 @@ while true; do
 
 	fails=0
 	for n in $net; do
-		if /bin/ping -c 1 "$n" -W 1 >/dev/null ; then
-			break
-		else
-			fails=$((fails+1)) && sleep 1
-		fi
+		tries=0
+		while [ $tries -lt 3 ]; do
+			if /bin/ping -c 1 "$n" -W 1 >/dev/null ; then
+				break 2
+			else
+				tries=$((tries+1))
+			fi
+		done
+		fails=$((fails+1))
 	done
 	[ $fails -eq $net_num ] && \
 	logger -st "scutclient_watchcat" "Connection failed, restart scutclient!" && \

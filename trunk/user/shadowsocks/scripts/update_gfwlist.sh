5725ae922b30b13d5a3d3ce8f3f2988378efb7f6
hanwckf <my375229675@gmail.com>
2018-10-25 01:19:58 +0800

    using generic gfwlist instead of ipset version, please reset /etc/storage

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -4,13 +4,15 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 GFWLIST_URL="$(nvram get gfwlist_url)"
+GFWLIST_NAME="$(nvram get gfwlist_name)"
+
 logger -st "gfwlist" "Starting update..."
 
-rm -f /tmp/dnsmasq_gfwlist_ipset.conf
-curl -k -s -o /tmp/dnsmasq_gfwlist_ipset.conf --connect-timeout 5 --retry 3 ${GFWLIST_URL:-https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf}
+rm -f /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"}
+curl -k -s -o /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"} --connect-timeout 5 --retry 3 ${GFWLIST_URL:-"https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf"}
 
 mkdir -p /etc/storage/gfwlist/
-mv -f /tmp/dnsmasq_gfwlist_ipset.conf /etc/storage/gfwlist/dnsmasq_gfwlist_ipset.conf
+mv -f /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"} /etc/storage/gfwlist/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"}
 
 mtd_storage.sh save >/dev/null 2>&1
 

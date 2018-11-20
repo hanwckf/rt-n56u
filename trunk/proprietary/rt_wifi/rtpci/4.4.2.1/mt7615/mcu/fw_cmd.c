7f041d40d85566cc32df175b1f3d607139e02540
hanwckf <my375229675@gmail.com>
2018-11-20 22:27:25 +0800

    mt7615: remove space

--- trunk/proprietary/rt_wifi/rtpci/4.4.2.1/mt7615/mcu/fw_cmd.c
+++ trunk/proprietary/rt_wifi/rtpci/4.4.2.1/mt7615/mcu/fw_cmd.c
@@ -3137,9 +3137,7 @@ static VOID bssUpdateBmcMngRate(
     CmdBssInfoBmcRate.u2Length = sizeof(CMD_BSSINFO_BMC_RATE_T);
 
     MTWF_LOG(DBG_CAT_FW, DBG_SUBCAT_ALL, DBG_LVL_OFF,
-            ("%s (BSS_INFO_BROADCAST_INFO), \
-                CmdBssInfoBmcRate.u2BcTransmit= %d, \
-                CmdBssInfoBmcRate.u2McTransmit = %d\n",
+            ("%s (BSS_INFO_BROADCAST_INFO), CmdBssInfoBmcRate.u2BcTransmit= %d, CmdBssInfoBmcRate.u2McTransmit = %d\n",
                 __FUNCTION__,
                 le2cpu16(CmdBssInfoBmcRate.u2BcTransmit),
                 le2cpu16(CmdBssInfoBmcRate.u2McTransmit)));

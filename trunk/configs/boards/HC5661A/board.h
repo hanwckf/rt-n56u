796b77cec3a7216012422a49deccbd0804dff601
hanwckf <my375229675@gmail.com>
2018-11-30 01:57:45 +0800

    add HC5661A (mt7628)

---
+++ trunk/configs/boards/HC5661A/board.h
@@ -0,0 +1,30 @@
+/* HC5661A */
+
+#define BOARD_PID		"HC5661A"
+#define BOARD_NAME		"HC5661A"
+#define BOARD_DESC		"HiWiFi HC5661A Wireless Router"
+#define BOARD_VENDOR_NAME	"HiWiFi"
+#define BOARD_VENDOR_URL	"http://www.hiwifi.com/"
+#define BOARD_MODEL_URL		"http://www.hiwifi.com/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_LED_INVERTED
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	44
+#define BOARD_GPIO_LED_POWER	37
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	1
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

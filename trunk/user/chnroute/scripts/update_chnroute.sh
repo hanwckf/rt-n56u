c67db94a73ae0e382292f108c936fb8775cbf195
hanwckf <my375229675@gmail.com>
2019-02-04 10:16:19 +0800

    chnroute: custom chnroute download url

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -3,9 +3,11 @@
 set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_chnroute)" != "1" ] && exit 0
+CHNROUTE_URL="$(nvram get chnroute_url)"
+
 logger -st "chnroute" "Starting update..."
 rm -f /tmp/chinadns_chnroute.txt
-wget -T 3 -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | \
+curl -k -s --connect-timeout 5 --retry 3 ${CHNROUTE_URL:-"http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"} | \
     awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > \
     /tmp/chinadns_chnroute.txt
 

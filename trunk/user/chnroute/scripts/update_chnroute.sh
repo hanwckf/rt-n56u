1a4ffffb4980f75a0c7966cd9d587b0b28a6ff73
hanwckf <my375229675@gmail.com>
2019-02-19 21:48:10 +0800

    chnroute: set chnroute_url to specify chnroute update URL

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -7,9 +7,13 @@ CHNROUTE_URL="$(nvram get chnroute_url)"
 
 logger -st "chnroute" "Starting update..."
 rm -f /tmp/chinadns_chnroute.txt
-curl -k -s --connect-timeout 5 --retry 3 ${CHNROUTE_URL:-"http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"} | \
-    awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > \
-    /tmp/chinadns_chnroute.txt
+
+if [ -z "$CHNROUTE_URL" ]; then
+	curl -k -s --connect-timeout 5 --retry 3 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | \
+		awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/chinadns_chnroute.txt
+else
+	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/chinadns_chnroute.txt "$CHNROUTE_URL"
+fi
 
 [ ! -d /etc/storage/chinadns/ ] && mkdir /etc/storage/chinadns/
 mv -f /tmp/chinadns_chnroute.txt /etc/storage/chinadns/chnroute.txt

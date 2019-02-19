0980bb4e6592de24b2015368ae4ab5735477ab61
hanwckf <my375229675@gmail.com>
2019-02-19 21:18:14 +0800

    gfwlist: simplified script

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -4,15 +4,14 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 GFWLIST_URL="$(nvram get gfwlist_url)"
-GFWLIST_NAME="$(nvram get gfwlist_name)"
 
 logger -st "gfwlist" "Starting update..."
 
-rm -f /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"}
-curl -k -s -o /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"} --connect-timeout 5 --retry 3 ${GFWLIST_URL:-"https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf"}
+rm -f /tmp/dnsmasq_gfwlist.conf
+curl -k -s -o /tmp/dnsmasq_gfwlist.conf --connect-timeout 5 --retry 3 ${GFWLIST_URL:-"https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist.conf"}
 
 mkdir -p /etc/storage/gfwlist/
-mv -f /tmp/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"} /etc/storage/gfwlist/${GFWLIST_NAME:-"dnsmasq_gfwlist.conf"}
+mv -f /tmp/dnsmasq_gfwlist.conf /etc/storage/gfwlist/dnsmasq_gfwlist.conf
 
 mtd_storage.sh save >/dev/null 2>&1
 

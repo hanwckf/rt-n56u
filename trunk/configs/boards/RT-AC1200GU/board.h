09673d69b9ea5e78267fbb148f6c2682ce1b4256
hanwckf <my375229675@gmail.com>
2019-07-20 20:39:27 +0800

    add support for RT-AC1200GU (@zxqwe)

---
+++ trunk/configs/boards/RT-AC1200GU/board.h
@@ -0,0 +1,32 @@
+/* ASUS RT-AC1200GU */
+
+#define BOARD_PID		"RT-AC1200GU"
+#define BOARD_NAME		"RT-AC1200GU"
+#define BOARD_DESC		"ASUS RT-AC1200GU Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"https://www.asus.com.cn/"
+#define BOARD_MODEL_URL		"https://www.asus.com.cn/Networking/RT-AC1200GU/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	41
+#define BOARD_GPIO_BTN_WPS	43
+#define BOARD_GPIO_LED_ALL	46
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	48
+#define BOARD_GPIO_LED_LAN	7
+#define BOARD_GPIO_LED_WAN	16
+#define BOARD_GPIO_LED_USB	47
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	1

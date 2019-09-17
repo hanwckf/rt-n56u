57bdf94b28a6aa42e601e953644c930a42aad37a
Yonsm <Yonsm@qq.com>
2019-09-17 11:59:04 +0800

    Add XY-C1 (without WIFI)

---
+++ trunk/configs/boards/XY-C1/board.h
@@ -0,0 +1,35 @@
+/* XY-C1/C5 */
+
+#define BOARD_PID		"XY-C1"
+#define BOARD_NAME		"XY-C1"
+#define BOARD_DESC		"XY-C1 Router"
+#define BOARD_VENDOR_NAME	"ChengDu JiZhi"
+#define BOARD_VENDOR_URL	"https://www.xyblock.net/"
+#define BOARD_MODEL_URL		"https://www.xyblock.net/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	3
+#define BOARD_GPIO_BTN_WPS	7
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define  BOARD_GPIO_LED_SW2G	14
+#define  BOARD_GPIO_LED_SW5G	16
+#define  BOARD_GPIO_LED_POWER	15
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	13
+#define BOARD_GPIO_LED_USB	10
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0

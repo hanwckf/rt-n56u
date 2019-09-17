74b4453e4f70ed2938bfad5d8382fae7fd58ad5f
Yonsm <Yonsm@qq.com>
2019-09-17 12:05:47 +0800

    Add shutdown button and refine logout/reboot icon

--- trunk/user/httpd/web_ex.c
+++ trunk/user/httpd/web_ex.c
@@ -3141,6 +3141,12 @@ apply_cgi(const char *url, webs_t wp)
 		sys_reboot();
 		return 0;
 	}
+	else if (!strcmp(value, " Shutdown "))
+	{
+		system("shutdown");
+		websRedirect(wp, current_url);
+		return 0;
+	}
 	else if (!strcmp(value, " RestoreNVRAM "))
 	{
 		websApply(wp, "Restarting.asp");

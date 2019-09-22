061ca4943f38f13a288333b0eb7309c3f4c599d8
hanwckf <my375229675@gmail.com>
2019-09-22 22:18:18 +0800

    remove linux kernel 3.0

--- trunk/linux-3.0.x/arch/arm/include/asm/memblock.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ASM_ARM_MEMBLOCK_H
-#define _ASM_ARM_MEMBLOCK_H
-
-struct meminfo;
-struct machine_desc;
-
-extern void arm_memblock_init(struct meminfo *, struct machine_desc *);
-
-#endif
--- trunk/linux-3.0.x/drivers/net/raeth/ra_mac.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RA_MAC_H__
-#define __RA_MAC_H__
-
-int  debug_proc_init(void);
-void debug_proc_exit(void);
-
-#endif

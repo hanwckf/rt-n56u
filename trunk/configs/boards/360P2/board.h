e9d7e9363156bc4f753c90c994e0bee2bf85f3f7
hanwckf <my375229675@gmail.com>
2019-10-14 21:56:41 +0800

    add support for 360P2

---
+++ trunk/configs/boards/360P2/board.h
@@ -0,0 +1,31 @@
+/* 360P2 */
+
+#define BOARD_PID		"360P2"
+#define BOARD_NAME		"360P2"
+#define BOARD_DESC		"360 P2 Wireless Router"
+#define BOARD_VENDOR_NAME	"netcore"
+#define BOARD_VENDOR_URL	"http://luyou.360.cn"
+#define BOARD_MODEL_URL		"http://luyou.360.cn"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_BTN_FN1	39
+#undef BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	44
+#define BOARD_GPIO_LED_POWER	11
+#undef  BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#define  BOARD_GPIO_LED_USB	46
+#define  BOARD_GPIO_LED_ROUTER	37
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

144ac2fac3d1e97e433bc542cb5693297663941f
effccb <effccb@hotmail.com>
2019-10-31 15:51:27 +0800

    add support for DIR-878

---
+++ trunk/configs/boards/DIR-878/board.h
@@ -0,0 +1,32 @@
+/* DIR-878 */
+
+#define BOARD_PID	"DIR-878"
+#define BOARD_NAME	"DIR-878"
+#define BOARD_DESC	"DIR-878 Wireless Router"
+#define BOARD_VENDOR_NAME	"DLINK"
+#define BOARD_VENDOR_URL	"http://www.dlink.com.cn/"
+#define BOARD_MODEL_URL	"http://www.dlink.com.cn/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_FN1	7
+#define BOARD_GPIO_BTN_RESET	15
+#define BOARD_GPIO_BTN_WPS	18
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define  BOARD_GPIO_LED_POWER	16
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	3
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX 4
+#define BOARD_NUM_ANT_5G_RX 4
+#define BOARD_NUM_ANT_2G_TX 4
+#define BOARD_NUM_ANT_2G_RX 4
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0

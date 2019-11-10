4d14535fed6a026e0ffaf4f7c4e9f7045347864b
hanwckf <my375229675@gmail.com>
2019-11-10 18:09:33 +0800

    libiconv: strip charsets

--- trunk/libs/libiconv/libiconv-1.16/lib/canonical_local.h
+++ trunk/libs/libiconv/libiconv-1.16/lib/canonical_local.h
@@ -1,2 +1,2 @@
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str193,
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str496,
+  (int)(long)&((struct stringpool_t *)0)->stringpool_str141,
+  (int)(long)&((struct stringpool_t *)0)->stringpool_str252,

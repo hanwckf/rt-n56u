75f0ce9a9de2194c77f19bfa51ad0857b4fcbd65
hanwckf <my375229675@gmail.com>
2019-12-19 01:25:39 +0800

    add support for MI-R3G (need test)

---
+++ trunk/configs/boards/MI-R3G/board.h
@@ -0,0 +1,31 @@
+/* Xiaomi Mi R3G */
+
+#define BOARD_PID		"MI-R3G"
+#define BOARD_NAME		"MI-R3G"
+#define BOARD_DESC		"Xiaomi Mi R3G Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3g"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
+#define BOARD_GPIO_LED_LAN	10
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	6
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
+#define BOARD_GPIO_PWR_USB	12	/* USB2.0 5V Power */
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1

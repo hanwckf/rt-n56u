364617f318b4f3f1dc88155311b36b0e15d8389b
Yonsm <Yonsm@qq.com>
2019-12-30 16:24:15 +0800

    Disable float log button

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -34,7 +34,7 @@ struct nvram_pair router_defaults[] = {
 
 	/* Miscellaneous parameters */
 	{ "time_zone", DEF_TIMEZONE },
-	{ "log_float_ui", "1" },		/* WebUI syslog float panel mode */
+	{ "log_float_ui", "0" },		/* WebUI syslog float panel mode */
 	{ "log_ipaddr", "" },			/* syslog recipient IP */
 	{ "log_port", "514" },			/* syslog recipient Port */
 	{ "log_level", "0" },			/* Bitmask 0:off 1:denied 2:accepted */

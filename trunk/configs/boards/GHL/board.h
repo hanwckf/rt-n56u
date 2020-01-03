ddb6b993b7a462177221fce645f3dfd480669b7e
chongshengB <306835258@qq.com>
2020-01-03 14:41:44 +0000

    add support to 歌华链

---
+++ trunk/configs/boards/GHL/board.h
@@ -0,0 +1,35 @@
+/* GHL */
+
+#define BOARD_PID		"GHL"
+#define BOARD_NAME		"GHL"
+#define BOARD_DESC		"GHL Wireless Router"
+#define BOARD_VENDOR_NAME	"Heaven"
+#define BOARD_VENDOR_URL	"http://www.bigwc.cn/"
+#define BOARD_MODEL_URL		"http://www.bigwc.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	3
+#define BOARD_GPIO_BTN_WPS	7
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI   
+#undef  BOARD_GPIO_LED_SW2G    	
+#undef  BOARD_GPIO_LED_SW5G	
+#define  BOARD_GPIO_LED_POWER	15
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	11
+#define BOARD_GPIO_LED_USB	10
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0

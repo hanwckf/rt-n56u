a86a10d216f94cba589773957fd7d1cd0759e1e5
chongshengB <306835258@qq.com>
2020-01-03 01:01:19 +0000

    add support to NEWIFI-D1

---
+++ trunk/configs/boards/NEWIFI-D1/board.h
@@ -0,0 +1,35 @@
+/* Lenovo NEWIFI-D1 */
+
+#define BOARD_PID		"NEWIFI-D1"
+#define BOARD_NAME		"NEWIFI-D1"
+#define BOARD_DESC		"Lenovo NEWIFI-D1 Wireless Router"
+#define BOARD_VENDOR_NAME	"Lenovo(Beijing) Limited"
+#define BOARD_VENDOR_URL	"http://www.newifi.com/"
+#define BOARD_MODEL_URL		"http://www.newifi.com/product_newifi2_summary.shtml"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	15
+#define BOARD_GPIO_BTN_WPS	18
+#define BOARD_GPIO_LED_ALL	13
+#define BOARD_GPIO_LED_WIFI 16
+#undef BOARD_GPIO_LED_SW2G		/* soft led */
+#undef BOARD_GPIO_LED_SW5G		/* soft led */
+#define BOARD_GPIO_LED_POWER	14
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB2	9
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
+

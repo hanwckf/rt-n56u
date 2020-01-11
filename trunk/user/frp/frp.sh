7003a8692a5759de97ed0418b0696dc1c996af31
chongshengB <306835258@qq.com>
2020-01-11 02:28:37 +0000

    fix frp.sh

--- trunk/user/frp/frp.sh
+++ trunk/user/frp/frp.sh
@@ -1,32 +1,72 @@
 #!/bin/sh
 frpc_enable=`nvram get frpc_enable`
 frps_enable=`nvram get frps_enable`
+http_username=`nvram get http_username`
 
-frp_start () {
-eval "/etc/storage/frp_script.sh" &
-restart_dhcpd
-[ ! -z "`pidof frpc`" ] && logger -t "frp" "frpc启动成功"
-[ ! -z "`pidof frps`" ] && logger -t "frp" "frps启动成功"
+check_frp () 
+{
+	check_net
+	result_net=$?
+	if [ "$result_net" = "1" ] ;then
+		if [ -z "`pidof frpc`" ] && [ "$frpc_enable" = "1" ];then
+			frp_start
+		fi
+		if [ -z "`pidof frps`" ] && [ "$frps_enable" = "1" ];then
+			frp_start
+		fi
+	fi
 }
 
-frp_close () {
-killall frpc frps frp_script.sh
-killall -9 frpc frps frp_script.sh
-restart_dhcpd
-[ -z "`pidof frpc`" ] && logger -t "frp" "停止 frpc"
-[ -z "`pidof frps`" ] && logger -t "frp" "停止 frps"
+check_net() 
+{
+	/bin/ping -c 3 223.5.5.5 -w 5 >/dev/null 2>&1
+	if [ "$?" == "0" ]; then
+		return 1
+	else
+		return 2
+		logger -t "frp" "检测到互联网未能成功访问,稍后再尝试启动frp"
+	fi
+}
+
+frp_start () 
+{
+	/etc/storage/frp_script.sh
+	sed -i '/frp/d' /etc/storage/cron/crontabs/$http_username
+	cat >> /etc/storage/cron/crontabs/$http_username << EOF
+*/1 * * * * /bin/sh /usr/bin/frp.sh C >/dev/null 2>&1
+EOF
+	[ ! -z "`pidof frpc`" ] && logger -t "frp" "frpc启动成功"
+	[ ! -z "`pidof frps`" ] && logger -t "frp" "frps启动成功"
+}
+
+frp_close () 
+{
+	if [ "$frpc_enable" = "0" ]; then
+		if [ ! -z "`pidof frpc`" ]; then
+		killall -9 frpc frp_script.sh
+		[ -z "`pidof frpc`" ] && logger -t "frp" "已停止 frpc"
+	    fi
+	fi
+	if [ "$frps_enable" = "0" ]; then
+		if [ ! -z "`pidof frps`" ]; then
+		killall -9 frps frp_script.sh
+		[ -z "`pidof frps`" ] && logger -t "frp" "已停止 frps"
+	    fi
+	fi
+	if [ "$frpc_enable" = "0" ] && [ "$frps_enable" = "0" ]; then
+	sed -i '/frp/d' /etc/storage/cron/crontabs/$http_username
+    fi
 }
 
 
 case $1 in
 start)
-	frp_close
 	frp_start
 	;;
 stop)
 	frp_close
 	;;
-*)
-
+C)
+	check_frp
 	;;
 esac

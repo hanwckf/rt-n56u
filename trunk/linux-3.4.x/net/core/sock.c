9b7b76cbef3d1d419c2cca77db2da179d8a54f96
hanwckf <my375229675@gmail.com>
2020-02-29 19:25:20 +0800

    kernel: backport SO_REUSEPORT support

--- trunk/linux-3.4.x/net/core/sock.c
+++ trunk/linux-3.4.x/net/core/sock.c
@@ -557,6 +557,9 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 	case SO_REUSEADDR:
 		sk->sk_reuse = valbool;
 		break;
+	case SO_REUSEPORT:
+		sk->sk_reuseport = valbool;
+		break;
 	case SO_TYPE:
 	case SO_PROTOCOL:
 	case SO_DOMAIN:
@@ -873,6 +876,10 @@ int sock_getsockopt(struct socket *sock, int level, int optname,
 
 	case SO_REUSEADDR:
 		v.val = sk->sk_reuse;
+		break;
+
+	case SO_REUSEPORT:
+		v.val = sk->sk_reuseport;
 		break;
 
 	case SO_KEEPALIVE:

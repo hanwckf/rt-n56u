84c98fbc21eee660478581e838e0510c33d4513a
chongshengB <306835258@qq.com>
2020-03-19 03:35:39 +0000

    SSP:fix gfwlist update

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
 set -e -o pipefail
-
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 #GFWLIST_URL="$(nvram get gfwlist_url)"
-
 logger -st "gfwlist" "Starting update..."
-
 rm -f /tmp/gfwlist_list.conf
 rm -f /etc/storage/gfwlist/gfwlist_list.conf
 curl -k -s -o /tmp/gfwlist_list.conf --connect-timeout 15 --retry 5 https://cokebar.github.io/gfwlist2dnsmasq/gfwlist_domain.txt
-
-mkdir -p /etc/storage/gfwlist/
-#logger -st "gfwlist" "Update......"
-if [ $(nvram get pdnsd_enable) = 0 ]; then
-awk '{printf("server=/%s/127.0.0.1#5353\nipset=/%s/gfwlist\n", $1, $1 )}' /tmp/gfwlist_list.conf > /etc/storage/gfwlist/gfwlist_list.conf
-else
-awk '{printf("ipset=/%s/gfwlist\n", $1, $1 )}' /tmp/gfwlist_list.conf > /etc/storage/gfwlist/gfwlist_list.conf
-fi
+count=`awk '{print NR}' /tmp/gfwlist_list.conf|tail -n1`
+if [ $count>1000 ]; then
+cp -r /tmp/gfwlist_list.conf /etc/storage/gfwlist/gfwlist_list.conf
 mtd_storage.sh save >/dev/null 2>&1
-
-/sbin/restart_dhcpd
-
+mkdir -p /etc/storage/gfwlist/
 logger -st "gfwlist" "Update done"
+#logger -st "gfwlist" "Update......"
+if [ $(nvram get ss_enable) = 1 ]; then
+logger -st "SS" "重启ShadowSocksR Plus+..."
+/usr/bin/shadowsocks.sh stop
+/usr/bin/shadowsocks.sh start
+fi
+else
+logger -st "gfwlist" "列表下载失败,请重试！"
+fi

2c1a8e75a635909d7a2b8efa6b759e5b7e7455ad
hanwckf <my375229675@gmail.com>
2020-03-28 09:31:10 +0800

    boards: add support for a3004ns

---
+++ trunk/configs/boards/A3004NS/board.h
@@ -0,0 +1,35 @@
+/* TOTOLINK A3004NS */
+
+#define BOARD_PID		"A3004NS"
+#define BOARD_NAME		"A3004NS"
+#define BOARD_DESC		"TOTOLINK A3004NS Wireless Router"
+#define BOARD_VENDOR_NAME	"TOTOLINK Limited"
+#define BOARD_VENDOR_URL	"http://www.totolink.cn/"
+#define BOARD_MODEL_URL		"http://www.totolink.cn/index.php/Product/show/73.html"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME		120
+#define BOARD_GPIO_BTN_RESET	4
+#define BOARD_GPIO_BTN_WPS	3
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_SW2G
+#undef BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	18
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	7
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB
+#undef BOARD_GPIO_PWR_USB2
+#undef BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
+

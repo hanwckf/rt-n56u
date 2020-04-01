8225d3b92d3c78d669e25ae92ded9002b295f980
hanwckf <my375229675@gmail.com>
2020-04-01 19:47:20 +0800

    boards: add support for hiwifi B70

---
+++ trunk/configs/boards/B70/board.h
@@ -0,0 +1,35 @@
+/* B70 */
+
+#define BOARD_PID		"B70"
+#define BOARD_NAME		"B70"
+#define BOARD_DESC		"B70 Wireless Router"
+#define BOARD_VENDOR_NAME	"HiWiFi/Gee Technology"
+#define BOARD_VENDOR_URL		"http://www.hiwifi.com/"
+#define BOARD_MODEL_URL		"http://www.hiwifi.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME		120
+#undef  BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET    18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	7
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	6
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB_ON   0
+#define BOARD_GPIO_PWR_USB      12
+#define BOARD_HAS_5G_11AC       1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY      4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	1

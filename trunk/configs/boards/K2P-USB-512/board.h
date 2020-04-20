6f879dd1d8c6eb6112d42d09e2df11a06128d0e7
chongshengB <306835258@qq.com>
2020-04-20 00:53:38 +0000

    add support to K2P-USB+16M+128M

---
+++ trunk/configs/boards/K2P-USB-512/board.h
@@ -0,0 +1,33 @@
+/* K2P-USB */
+
+#define BOARD_PID		"K2P-USB"
+#define BOARD_NAME		"K2P-USB"
+#define BOARD_DESC		"PHICOMM K2P Wireless Router"
+#define BOARD_VENDOR_NAME	"PHICOMM"
+#define BOARD_VENDOR_URL	"http://www.phicomm.com/"
+#define BOARD_MODEL_URL		"http://www.phicomm.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#undef BOARD_GPIO_BTN_RESET
+#define BOARD_GPIO_BTN_WPS	3
+#undef  BOARD_GPIO_LED_ALL
+#define  BOARD_GPIO_LED_WIFI	14
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define  BOARD_GPIO_LED_POWER	13
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	15
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0

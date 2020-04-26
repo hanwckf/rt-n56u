8b8ce59bcb4d0c1f68d1cca0aaa15714fb4743f5
240038901 <58842348+240038901@users.noreply.github.com>
2020-04-26 21:47:19 +0800

    Create board.h

---
+++ trunk/configs/boards/ZTE-E8820/board.h
@@ -0,0 +1,35 @@
+/* NEWIFI3(d2) */
+
+#define BOARD_PID		"ZTE-E8820"
+#define BOARD_NAME		"ZTE-E8820"
+#define BOARD_DESC		"ZTE-E8820 Wireless Router"
+#define BOARD_VENDOR_NAME	"ZTE Technology"
+#define BOARD_VENDOR_URL	"http://www.ztehome.com.cn/"
+#define BOARD_MODEL_URL		"http://www.ztehome.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	18
+#define BOARD_GPIO_BTN_WPS	24
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI  29
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define  BOARD_GPIO_LED_POWER	 31
+#undef  BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN	
+#undef BOARD_GPIO_LED_USB	
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0

16b102d78202d96573f6a0ed090b39c20bca0756
emirefek <emirefek@gmail.com>
2020-06-14 17:01:52 +0300

    Xiaomi R2100 Fix Power/Internet LEDs (#398)
    I was defined them wrong at my previous PR. This should fix them.

--- trunk/configs/boards/R2100/board.h
+++ trunk/configs/boards/R2100/board.h
@@ -12,9 +12,9 @@
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
-#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
+#define BOARD_GPIO_LED_POWER	12	/* 6: red, 10: yellow, 8: blue */
 #undef  BOARD_GPIO_LED_LAN
-#define  BOARD_GPIO_LED_WAN	14
+#define  BOARD_GPIO_LED_WAN	8
 #undef  BOARD_GPIO_LED_USB	
 #undef  BOARD_GPIO_LED_ROUTER
 #undef BOARD_GPIO_PWR_USB_ON	

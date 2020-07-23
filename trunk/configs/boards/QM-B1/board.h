e86b1255ce9ecf6f9dbcb8bc71418c9da449303f
monw <65690955+monw@users.noreply.github.com>
2020-07-23 10:56:52 +0800

    Create board.h

---
+++ trunk/configs/boards/QM-B1/board.h
@@ -0,0 +1,36 @@
+/* QM-B1 */
+
+#define BOARD_PID		"QM-B1"
+#define BOARD_NAME		"QM-B1"
+#define BOARD_DESC		"QM-B1 Wireless Router"
+#define BOARD_VENDOR_NAME	"QM-B1 Technology"
+#define BOARD_VENDOR_URL	"http://www.qianmo.com.cn/"
+#define BOARD_MODEL_URL		"http://www.qianmo.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	18
+#undef BOARD_GPIO_BTN_WP
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI  
+#undef BOARD_GPIO_LED_SW2G   
+#undef BOARD_GPIO_LED_SW5G   
+#undef BOARD_GPIO_LED_POWER	
+#define BOARD_GPIO_LED_SYS	29
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN	
+#undef BOARD_GPIO_LED_USB	
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0

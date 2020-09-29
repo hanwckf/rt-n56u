1bac6bf0f1a758f9b5ce90ad2f8d97b17629deff
hanwckf <hanwckf@vip.qq.com>
2020-09-29 13:23:50 +0800

    mt7615-5.0.3.0: fix AutoChannelSelect in DBDC mode

--- trunk/proprietary/rt_wifi/rtpci/5.0.3.0/mt7615/embedded/common/cmm_profile.c
+++ trunk/proprietary/rt_wifi/rtpci/5.0.3.0/mt7615/embedded/common/cmm_profile.c
@@ -362,6 +362,7 @@ static VOID RTMPChannelCfg(RTMP_ADAPTER *pAd, RTMP_STRING *Buffer)
 #endif /* CONFIG_AP_SUPPORT */
 	}
 
+#ifndef DBDC_MODE
 #ifdef CONFIG_AP_SUPPORT
 #ifdef MBSS_SUPPORT
 	/*Can not assign default channel to wdev-> channel when channel = 0 */
@@ -382,6 +383,7 @@ static VOID RTMPChannelCfg(RTMP_ADAPTER *pAd, RTMP_STRING *Buffer)
 	}
 #endif/*MBSS_SUPPORT*/
 #endif /*CONFIG_AP_SUPPORT*/
+#endif
 
 }
 
@@ -3495,6 +3497,7 @@ NDIS_STATUS	RTMPSetProfileParameters(
 
 #ifdef CONFIG_AP_SUPPORT
 		/*AutoChannelSelect*/
+		pAd->ApCfg.AutoChannelAlg = 3; //force using busytime ACS alg
 		if (RTMPGetKeyParameter("AutoChannelSelect", tmpbuf, 10, pBuffer, TRUE)) {
 			if (os_str_tol(tmpbuf, 0, 10) != 0) { /*Enable*/
 				ChannelSel_Alg SelAlg = (ChannelSel_Alg)os_str_tol(tmpbuf, 0, 10);
@@ -3503,7 +3506,6 @@ NDIS_STATUS	RTMPSetProfileParameters(
 					pAd->ApCfg.bAutoChannelAtBootup = FALSE;
 				else { /*Enable*/
 					pAd->ApCfg.bAutoChannelAtBootup = TRUE;
-					pAd->ApCfg.AutoChannelAlg = 3; //force using busytime ACS alg
 				}
 			} else /*Disable*/
 				pAd->ApCfg.bAutoChannelAtBootup = FALSE;

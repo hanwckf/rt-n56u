f7565d2aec9144f84e86887ce808794fc065d0ad
Alexey <@dm38>
2020-10-13 22:11:44 +0800

    ext4: fix the number of credits needed for ext4_ext_migrate()    The migration ioctl creates a temporary inode.  Since this inode is    never linked to a directory, we don't need to reserve journal credits    required for modifying the directory.

--- trunk/linux-3.4.x/fs/ext4/migrate.c
+++ trunk/linux-3.4.x/fs/ext4/migrate.c
@@ -455,11 +455,13 @@ int ext4_ext_migrate(struct inode *inode)
 		 */
 		return retval;
 
+	/*
+	 * Worst case we can touch the allocation bitmaps, a bgd
+	 * block, and a block to link in the orphan list.  We do need
+	 * need to worry about credits for modifying the quota inode.
+	 */
 	handle = ext4_journal_start(inode,
-					EXT4_DATA_TRANS_BLOCKS(inode->i_sb) +
-					EXT4_INDEX_EXTRA_TRANS_BLOCKS + 3 +
-					EXT4_MAXQUOTAS_INIT_BLOCKS(inode->i_sb)
-					+ 1);
+					4 + EXT4_MAXQUOTAS_TRANS_BLOCKS(inode->i_sb));
 	if (IS_ERR(handle)) {
 		retval = PTR_ERR(handle);
 		return retval;

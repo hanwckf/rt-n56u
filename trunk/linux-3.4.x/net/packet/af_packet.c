3b2e0a6e3774205f04593f6f911e588997e3d251
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2020-10-14 10:00:54 +0800

    kernel: packet: in packet_do_bind, test fanout with bind_lock held commit 4971613c1639d8e5f102c4e797c3bf8f83a5a69e upstream.
    Once a socket has po->fanout set, it remains a member of the group
    until it is destroyed. The prot_hook must be constant and identical
    across sockets in the group.
    
    If fanout_add races with packet_do_bind between the test of po->fanout
    and taking the lock, the bind call may make type or dev inconsistent
    with that of the fanout group.
    
    Hold po->bind_lock when testing po->fanout to avoid this race.
    
    I had to introduce artificial delay (local_bh_enable) to actually
    observe the race.
    
    Fixes: dc99f600698d ("packet: Add fanout support.")
    Signed-off-by: default avatarWillem de Bruijn <willemb@google.com>
    Reviewed-by: default avatarEric Dumazet <edumazet@google.com>
    Signed-off-by: default avatarDavid S. Miller <davem@davemloft.net>
    Signed-off-by: Ben Hutchings's avatarBen Hutchings <ben@decadent.org.uk>

--- trunk/linux-3.4.x/net/packet/af_packet.c
+++ trunk/linux-3.4.x/net/packet/af_packet.c
@@ -1326,7 +1326,7 @@ static int fanout_add(struct sock *sk, u16 id, u16 type_flags)
 		list_add(&match->list, &fanout_list);
 	}
 	err = -EINVAL;
-	
+
 	spin_lock(&po->bind_lock);
 	if (po->running &&
 	    match->type == type &&
@@ -2529,12 +2529,14 @@ static int packet_do_bind(struct sock *sk, const char *name, int ifindex,
 	int ret = 0;
 	bool unlisted = false;
 
-	if (po->fanout)
-		return -EINVAL;
-
 	lock_sock(sk);
 	spin_lock(&po->bind_lock);
 	rcu_read_lock();
+
+	if (po->fanout) {
+		ret = -EINVAL;
+		goto out_unlock;
+	}
 
 	if (name) {
 		dev = dev_get_by_name_rcu(sock_net(sk), name);

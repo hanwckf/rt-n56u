c5d019088ce3cb5faf38c236b1ccee8411a1fd1d
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2020-10-17 19:03:19 +0800

    kernel: net: bridge: deny dev_set_mac_address() when unregistering     commit c4b4c421857dc7b1cf0dccbd738472360ff2cd70 upstream.
        We have an interesting memory leak in the bridge when it is being
        unregistered and is a slave to a master device which would change the
        mac of its slaves on unregister (e.g. bond, team). This is a very
        unusual setup but we do end up leaking 1 fdb entry because
        dev_set_mac_address() would cause the bridge to insert the new mac address
        into its table after all fdbs are flushed, i.e. after dellink() on the
        bridge has finished and we call NETDEV_UNREGISTER the bond/team would
        release it and will call dev_set_mac_address() to restore its original
        address and that in turn will add an fdb in the bridge.
        One fix is to check for the bridge dev's reg_state in its
        ndo_set_mac_address callback and return an error if the bridge is not in
        NETREG_REGISTERED.

--- trunk/linux-3.4.x/net/bridge/br_device.c
+++ trunk/linux-3.4.x/net/bridge/br_device.c
@@ -172,6 +172,12 @@ static int br_set_mac_address(struct net_device *dev, void *p)
 	if (!is_valid_ether_addr(addr->sa_data))
 		return -EADDRNOTAVAIL;
 
+	/* dev_set_mac_addr() can be called by a master device on bridge's
+	 * NETDEV_UNREGISTER, but since it's being destroyed do nothing
+	 */
+	if (dev->reg_state != NETREG_REGISTERED)
+		return -EBUSY;
+
 	spin_lock_bh(&br->lock);
 	if (compare_ether_addr(dev->dev_addr, addr->sa_data)) {
 		dev->addr_assign_type &= ~NET_ADDR_RANDOM;

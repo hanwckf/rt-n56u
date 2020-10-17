13f35bc3899de77e76e147b7297e35e505908846
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2020-10-17 17:02:10 +0800

    kernel: inet: stop leaking jiffies on the wire commit a904a0693c189691eeee64f6c6b188bd7dc244e9 upstream.
    Historically linux tried to stick to RFC 791, 1122, 2003
    for IPv4 ID field generation.
    
    RFC 6864 made clear that no matter how hard we try,
    we can not ensure unicity of IP ID within maximum
    lifetime for all datagrams with a given source
    address/destination address/protocol tuple.
    
    Linux uses a per socket inet generator (inet_id), initialized
    at connection startup with a XOR of 'jiffies' and other
    fields that appear clear on the wire.
    
    Thiemo Nagel pointed that this strategy is a privacy
    concern as this provides 16 bits of entropy to fingerprint
    devices.
    
    Let's switch to a random starting point, this is just as
    good as far as RFC 6864 is concerned and does not leak
    anything critical.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Signed-off-by: default avatarEric Dumazet <edumazet@google.com>
    Reported-by: default avatarThiemo Nagel <tnagel@google.com>
    Signed-off-by: default avatarDavid S. Miller <davem@davemloft.net>
    [bwh: Backported to 3.16: drop changes in chelsio]
    Signed-off-by: Ben Hutchings's avatarBen Hutchings <ben@decadent.org.uk>

--- trunk/linux-3.4.x/net/ipv4/tcp_ipv4.c
+++ trunk/linux-3.4.x/net/ipv4/tcp_ipv4.c
@@ -1430,7 +1430,7 @@ struct sock *tcp_v4_syn_recv_sock(struct sock *sk, struct sk_buff *skb,
 	inet_csk(newsk)->icsk_ext_hdr_len = 0;
 	if (inet_opt)
 		inet_csk(newsk)->icsk_ext_hdr_len = inet_opt->opt.optlen;
-	newinet->inet_id = newtp->write_seq ^ jiffies;
+	newinet->inet_id = prandom_u32();
 
 	if (!dst) {
 		dst = inet_csk_route_child_sock(sk, newsk, req);

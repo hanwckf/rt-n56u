1e17683ee2c96a02afb01af3a7fe5e7593722c51
Alexey <@dm38>
2020-10-25 21:10:10 +0800

    kernel: net/packet: Added lost part of "fix overflow in tpacket_rcv"

--- trunk/linux-3.4.x/net/packet/af_packet.c
+++ trunk/linux-3.4.x/net/packet/af_packet.c
@@ -1775,6 +1775,7 @@ static int tpacket_rcv(struct sk_buff *skb, struct net_device *dev,
 		macoff = netoff - maclen;
 	}
 	if (netoff > USHRT_MAX) {
+		po->stats.tp_drops++;
 		goto drop_n_restore;
 	}
 	if (po->tp_version <= TPACKET_V2) {

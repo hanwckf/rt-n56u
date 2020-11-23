d3103a8592aeb6c8e18a674ee637baaaab6eed71
JuanLinn <64671466+JuanLinn@users.noreply.github.com>
2020-11-23 16:07:25 -0500

    Update detect_internet.c

--- trunk/user/rc/detect_internet.c
+++ trunk/user/rc/detect_internet.c
@@ -276,7 +276,7 @@ di_on_timer(void)
 #if defined (BOARD_GPIO_LED_WAN)
 		if (nvram_get_int("front_led_wan") == 3){
 			LED_CONTROL(BOARD_GPIO_LED_WAN, (link_internet) ? LED_ON : LED_OFF);
-#if ((BOARD_GPIO_LED_POWER == 13 && BOARD_GPIO_LED_WIFI == 14 && BOARD_GPIO_LED_WAN ==15) || (BOARD_GPIO_LED_POWER == 8 && BOARD_GPIO_LED_WIFI == 11 && BOARD_GPIO_LED_WAN ==10))
+#if defined (BOARD_K2P) || defined (BOARD_PSG1218)
 			LED_CONTROL(BOARD_GPIO_LED_WIFI, (link_internet) ? LED_OFF : LED_ON);
 #endif
 		}

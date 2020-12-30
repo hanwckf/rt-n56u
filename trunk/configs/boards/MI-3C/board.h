306bbf90e54f4ae78680901d9b5d2ccbcdfc1dc4
root <root@administrator>
2020-12-30 14:07:04 +0100

    Include XIAOMI Mi-3C config files

---
+++ trunk/configs/boards/MI-3C/board.h
@@ -0,0 +1,26 @@
+/* Xiaomi Mi 3C (R3C)*/
+
+#define BOARD_PID		"MI-3C"
+#define BOARD_NAME		"MI-3C"
+#define BOARD_DESC		"Xiaomi Mi 3C Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifi3c"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI 	11
+#define BOARD_GPIO_LED_POWER 	24 	/* 24: blue, 26: yellow, 29: red */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

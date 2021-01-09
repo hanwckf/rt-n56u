558fa2f18827cd42e4c4b3ccf1a83415eeb28611
liuyunwuhen <liuyunwuhen@gmail.com>
2021-01-09 09:40:22 +0800

    boards: add support for zte-e8820s (#551)

---
+++ trunk/configs/boards/ZTE_E8820S/board.h
@@ -0,0 +1,36 @@
+/* ZTE_E8820S */
+
+#define BOARD_PID		"ZTE_E8820S"
+#define BOARD_NAME		"ZTE_E8820S"
+#define BOARD_DESC		"ZTE_E8820S Wireless Router"
+#define BOARD_VENDOR_NAME	"ZTE Technology"
+#define BOARD_VENDOR_URL	"http://www.ztehome.com.cn/"
+#define BOARD_MODEL_URL		"http://www.ztehome.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME   	120
+#define BOARD_GPIO_BTN_FN1    10
+#define BOARD_GPIO_BTN_RESET	18
+#define BOARD_GPIO_BTN_WPS	8
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI  
+#undef BOARD_GPIO_LED_SW2G   
+#undef BOARD_GPIO_LED_SW5G   
+#define BOARD_GPIO_LED_POWER	3
+#define BOARD_GPIO_LED_SYS	
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN	
+#undef BOARD_GPIO_LED_USB	
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0

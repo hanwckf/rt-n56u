485f2ef4060b3a66a6f14129d40709615e863cdd
padavanonly <83120842+padavanonly@users.noreply.github.com>
2021-04-24 20:48:27 +0800

    boards: Add new device (#628)
    add xiaomi-R4A, NETGEAR R6220, NETGEAR R6800
    
    Co-authored-by: root <meisreallyba@gmail.com>

---
+++ trunk/configs/boards/NETGEAR-BZV/board.h
@@ -0,0 +1,32 @@
+/* NETGEAR-BZV */
+
+#define BOARD_PID		"NETGEAR-BZV"
+#define BOARD_NAME		"NETGEAR-BZV"
+#define BOARD_DESC		"NETGEAR Dual MT7615 Wireless Router"
+#define BOARD_VENDOR_NAME	"NETGEAR Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.netgear.com/"
+#define BOARD_MODEL_URL		"http://www.netgear.com/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	12
+#define BOARD_GPIO_BTN_WPS	18
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	14
+#define BOARD_GPIO_LED_POWER	0	/* 0: orange, 1: white */
+#define BOARD_GPIO_LED_LAN	9	/* 0: orange, 1: white */
+#define BOARD_GPIO_LED_WAN	3	/* 0: orange, 1: white */
+#define BOARD_GPIO_LED_USB	7
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON	
+#undef  BOARD_GPIO_PWR_USB	
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	4
+#define BOARD_NUM_ANT_5G_RX	4
+#define BOARD_NUM_ANT_2G_TX	4
+#define BOARD_NUM_ANT_2G_RX	4
+#define BOARD_NUM_ETH_LEDS	5
+#define BOARD_NUM_ETH_EPHY	5
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+

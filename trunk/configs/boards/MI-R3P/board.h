94edf6d47b075e22b0c2e3f9c4f0d191f735b6c9
padavanonly <83120842+padavanonly@users.noreply.github.com>
2021-04-25 09:12:08 +0800

    boards: add Xiaomi-R3P (#630)
    Co-authored-by: root <meisreallyba@gmail.com>

---
+++ trunk/configs/boards/MI-R3P/board.h
@@ -0,0 +1,31 @@
+/* Xiaomi Mi R3P */
+
+#define BOARD_PID		"MI-R3P"
+#define BOARD_NAME		"MI-R3P"
+#define BOARD_DESC		"Xiaomi Mi R3P Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3p"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
+#undef  BOARD_GPIO_LED_LAN	
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB	
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
+#define BOARD_GPIO_PWR_USB	12	/* USB2.0 5V Power */
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	4
+#define BOARD_NUM_ANT_5G_RX	4
+#define BOARD_NUM_ANT_2G_TX	4
+#define BOARD_NUM_ANT_2G_RX	4
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1

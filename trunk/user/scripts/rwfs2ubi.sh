451a69d2e05eaf6ffde3936da149508b2d831220
hanwckf <hanwckf@vip.qq.com>
2021-05-08 22:13:09 +0800

    rwfs2ubi.sh: fix typo

--- trunk/user/scripts/rwfs2ubi.sh
+++ trunk/user/scripts/rwfs2ubi.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -x /sbin/ubiattach ] && \
-	[ ! -x /sbin/ubiformat ] && \
-	[ ! -x /sbin/ubimkvol ] && \
+if [ ! -x /sbin/ubiattach ] || \
+	[ ! -x /sbin/ubiformat ] || \
+	[ ! -x /sbin/ubimkvol ] || \
 	[ ! -x /sbin/ubidetach ]; then
-  echo "Unable to find the ubi utils executables!" >&2
+  echo "Unable to find the UBI utils executables!" >&2
   exit 1
 fi
 
@@ -30,14 +30,14 @@ mtd_write erase /dev/mtd${rwfs_idx}
 ubiformat /dev/mtd${rwfs_idx} -y -q
 
 if [ $? != 0 ]; then
-	echo "Format RWFS partition (/dev/mtd${rwfs_idx}) to UBIFS FAILED!"
+	echo "Format RWFS partition (/dev/mtd${rwfs_idx}) to UBIFS FAILED!" >&2
 	exit 1
 fi
 
 ubiattach -p /dev/mtd${rwfs_idx} -d ${rwfs_idx}
 
 if [ $? != 0 ]; then
-	echo "Attach UBIFS (/dev/mtd${rwfs_idx}) FAILED!"
+	echo "Attach UBIFS (/dev/mtd${rwfs_idx}) FAILED!" >&2
 	exit 1
 fi
 
@@ -50,7 +50,7 @@ if [ $? = 0 ]; then
 	echo "If you want to automount RWFS to /opt, please create 'opt' directory in '/media/mtd_rwfs', then reboot the router."
 	echo "If you want to setup Entware in /opt, please run 'nvram set optw_enable=2 && nvram commit', then run 'opt-start.sh' to download Entware installer."
 else
-	echo "Create UBI volume FAILED!"
+	echo "Create UBI volume FAILED!" >&2
 fi
 
 ubidetach -p /dev/mtd${rwfs_idx}

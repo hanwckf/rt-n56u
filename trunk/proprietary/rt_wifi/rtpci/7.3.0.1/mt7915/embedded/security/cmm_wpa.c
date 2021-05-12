a7d4cd52238a38afa14eabb8ec87b13446ec46c5
hanwckf <hanwckf@vip.qq.com>
2021-05-12 13:27:20 +0800

    mt7915: remove noisy log

--- trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/security/cmm_wpa.c
+++ trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/security/cmm_wpa.c
@@ -6916,7 +6916,7 @@ static VOID WpaEAPOLRetryAction(
 			} else if (pHandshake->MsgType == EAPOL_GROUP_MSG_1) {
 				if (pHandshake->MsgRetryCounter > GROUP_MSG1_RETRY_LIMIT) {
 					pHandshake->GTKState = REKEY_FAILURE;
-					MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_ERROR, ("%s::Group rekey timeout from %02X:%02X:%02X:%02X:%02X:%02X\n", __func__, PRINT_MAC(pHandshake->SAddr)));
+					MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_INFO, ("%s::Group rekey timeout from %02X:%02X:%02X:%02X:%02X:%02X\n", __func__, PRINT_MAC(pHandshake->SAddr)));
 				} else {
 					WPABuildGroupMsg1(pAd, &pEntry->SecConfig, pEntry);
 					MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_TRACE, ("%s::ReTry MSG1 of 2-way Handshake, Counter = %d\n", __func__, pHandshake->MsgRetryCounter));

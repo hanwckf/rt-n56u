4e916145672821d07e22481abb070f9769dc6d27
hanwckf <hanwckf@vip.qq.com>
2021-05-18 20:05:42 +0800

    tools: update depmod.sh

--- trunk/proprietary/rt_wifi/rtpci/4.4.2.1/mt7615/embedded/tx_rx/wdev_rx.c
+++ trunk/proprietary/rt_wifi/rtpci/4.4.2.1/mt7615/embedded/tx_rx/wdev_rx.c
@@ -1674,7 +1674,7 @@ static INT rtmp_chk_rx_err(RTMP_ADAPTER *pAd, RX_BLK *pRxBlk)
 
 		if (pRxBlk->DeAmsduFail)
 		{
-			MTWF_LOG(DBG_CAT_RX, DBG_SUBCAT_ALL, DBG_LVL_OFF, 
+			MTWF_LOG(DBG_CAT_RX, DBG_SUBCAT_ALL, DBG_LVL_INFO, 
 				("Deammsdu Fail, WlanIndex = %d\n", rxd_base->RxD2.RxDWlanIdx));
 			return NDIS_STATUS_FAILURE;
 		}

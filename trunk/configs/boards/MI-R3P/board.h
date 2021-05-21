0e43df6ab301f625a2972ba33bd399376a500b80
blioild <56418022+blioild@users.noreply.github.com>
2021-05-21 09:46:31 +0800

    MI-R3P: fix front leds

--- trunk/configs/boards/MI-R3P/board.h
+++ trunk/configs/boards/MI-R3P/board.h
@@ -13,9 +13,9 @@
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
 #define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
-#undef  BOARD_GPIO_LED_LAN	
+#define  BOARD_GPIO_LED_LAN	10
 #undef  BOARD_GPIO_LED_WAN
-#undef  BOARD_GPIO_LED_USB	
+#define  BOARD_GPIO_LED_USB	6
 #undef  BOARD_GPIO_LED_ROUTER
 #define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
 #define BOARD_GPIO_PWR_USB	12	/* USB2.0 5V Power */

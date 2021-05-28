7207f2102d499ed3ad6202ff6d1b96fe88208696
hanwckf <hanwckf@vip.qq.com>
2021-05-29 00:34:47 +0800

    boards: fix board CFLAGS

--- trunk/configs/boards/R6220/board.mk
+++ trunk/configs/boards/R6220/board.mk
@@ -4,5 +4,5 @@
 # R6220     # R6220            # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_NETGEAR
+CFLAGS += -DBOARD_NETGEAR_R6220
 BOARD_NUM_USB_PORTS=1

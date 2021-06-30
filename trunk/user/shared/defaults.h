2d79befd9a0ecc8b4cb83169f69d5a38c1b6de85
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2021-06-30 22:53:21 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -30,9 +30,9 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.2.1"
-#define DEF_LAN_DHCP_BEG	"192.168.2.100"
-#define DEF_LAN_DHCP_END	"192.168.2.244"
+#define DEF_LAN_ADDR		"10.10.10.1"
+#define DEF_LAN_DHCP_BEG	"10.10.10.100"
+#define DEF_LAN_DHCP_END	"10.10.10.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"

66e3764e0ecf21c261d30ec9f932b79144043cb9
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2021-07-03 00:52:29 +0800

    Update board.mk

--- trunk/configs/boards/5K-W20/board.mk
+++ trunk/configs/boards/5K-W20/board.mk
@@ -4,5 +4,5 @@
 # 5K-W20    # 5K-W20           # MT7620  #
 ##################################################################
 
-CFLAGS += -DBOARD_GL-MT300N
+CFLAGS += -DBOARD_5K_W20
 BOARD_NUM_USB_PORTS=1

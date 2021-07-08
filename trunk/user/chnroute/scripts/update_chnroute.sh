d8f478141f40ca36e551998804ecdf53a9babf8c
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2021-07-09 07:46:54 +0800

    Update update_chnroute.sh

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -9,7 +9,7 @@ logger -st "chnroute" "Starting update..."
 rm -f /tmp/chinadns_chnroute.txt
 
 if [ -z "$CHNROUTE_URL" ]; then
-	curl -k -s --connect-timeout 20 --retry 3 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | \
+	curl -k -s --connect-timeout 20 --retry 3 https://raw.githubusercontent.com/xiaiohuan/rt-n56u/master/trunk/user/chnroute/chnroute.txt.tmp | \
 		awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/chinadns_chnroute.txt
 else
 	curl -k -s --connect-timeout 20 --retry 3 -o /tmp/chinadns_chnroute.txt "$CHNROUTE_URL"

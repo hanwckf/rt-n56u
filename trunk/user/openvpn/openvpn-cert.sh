1f4149f99b2002c37f7c279bc409e092a6e4d580
baiduerzi <31235783+baiduerzi@users.noreply.github.com>
2021-07-10 01:00:17 +0800

    Add files via upload

--- trunk/user/openvpn/openvpn-cert.sh
+++ trunk/user/openvpn/openvpn-cert.sh
@@ -22,6 +22,7 @@ SERVER_CRT=server.crt
 CLIENT_KEY=client.key
 CLIENT_CRT=client.crt
 TA_KEY=ta.key
+STC2_KEY=stc2.key
 ECPARAM=ecparam.pem
 ## number of bits to use when generate new key
 RSA_BITS=1024
@@ -37,6 +38,10 @@ CRT_PATH_SRV="$CRT_PATH/server"
 if [ -z "$CRT_PATH_CLI" ] ; then
   CRT_PATH_CLI="$CRT_PATH/client"
 fi
+
+# Check if -sha256 is supported
+DGST_ALG="-sha1"
+openssl list -1 --digest-commands 2>&1 | grep -q 'sha256' && DGST_ALG="-sha256"
 
 func_help() {
   local BOLD="echo -ne \\033[1m"
@@ -55,7 +60,7 @@ func_help() {
   echo "           - root CA key and certificate" >&2
   echo "           - server key and certificate" >&2
   echo "           - Diffie-Hellman parameters key" >&2
-  echo "           - TLS-Auth/TLS-Crypt HMAC signature key" >&2
+  echo "           - TLS-Auth HMAC signature key" >&2
   echo "           `$BOLD`Note:`$NORM` $CA_CRT and ${TA_KEY}(if TLS-Auth or TLS-Crypt is used) should be sent to clients." >&2
   echo >&2
   echo "    `$BOLD`client`$NORM` -n `$BOLD`common_name`$NORM` [ -b `$BOLD`rsa_bits/ec_name`$NORM` ] [ -d `$BOLD`days_valid`$NORM` ]" >&2
@@ -140,23 +145,18 @@ make_cert() {
   # $3 --> days valid
   # $4 --> rsa bits
   # $5 --> CN
-  # $6 --> ca if cert is CA
+  # $6 --> signature algorithm
+  # $7 --> ca if cert is CA
   #
-  [ "$6" == "ca" ] && local CA_TRUE="-x509"
-  if [ -s $1 ] ; then
+  [ "$7" == "ca" ] && local CA_TRUE="-x509"
+    if  [ ! -s $1 ] ; then
+         [[ `echo $4 | grep '^[bpsw]'` ]] && openssl ecparam -name $4 -genkey -out $1
+         [[ `echo $4 | grep '^ed'` ]] && openssl genpkey -algorithm $4 -out $1
+         [[ `echo $4 | grep '^[1-9]'` ]] && openssl genrsa -out $1 $4
+    fi
     echo_process "Creating ${2}: $5"
     openssl req -nodes $CA_TRUE -days $3 -new -outform PEM \
-            -out $2 -key $1 -sha1 -subj "/CN=$5" &>/dev/null
-  else
-    local C_PARAM="rsa:$4"
-    if echo $4 | grep -q '^[bpsw]'; then
-	openssl ecparam -name "$RSA_BITS" -out "$ECPARAM"
-	C_PARAM="ec:$ECPARAM"
-    fi
-    echo_process "Creating new ${2}: $5"
-    openssl req -nodes $CA_TRUE -days $3 -newkey $C_PARAM \
-            -outform PEM -out $2 -keyout $1 -sha1 -subj "/CN=$5" &>/dev/null
-  fi
+            -out $2 -key $1 $6 -subj "/CN=$5" &>/dev/null
   [ -f $1 ] && chmod 600 $1
   echo_done
 }
@@ -168,7 +168,8 @@ sign_cert() {
   # $3 --> csr input file name
   # $4 --> crt output file name
   # $5 --> days valid
-  # $6 --> extensions to use (server or client)
+  # $6 --> signature algorithm
+  # $7 --> extensions to use (server or client)
   #
   if [ ! -f $1 ] || [ ! -f $2 ] ; then
     echo "Error: CA not found" >&2
@@ -177,8 +178,8 @@ sign_cert() {
   write_ext_cfs
   echo_process "Signing $4"
   openssl x509 -req -in $3 -CA $2 -CAkey $1 -CAcreateserial \
-               -clrext -out $4 -sha1 -extfile $SSL_EXT_FILE \
-               -days $5 -extensions $6 &>/dev/null
+               -clrext -out $4 -$6 -extfile $SSL_EXT_FILE \
+               -days $5 -extensions $7 &>/dev/null
   rm -f $3
   rm -f ca.srl
   echo_done
@@ -197,10 +198,13 @@ make_dh() {
   echo_done
 }
 
-make_ta() {
+make_ta_tc2() {
   #
   # $1 --> ta key name
   #
+  # $2 --> server tc2 key name
+  #
+
   if [ ! -x $OPENVPN ] ; then
     echo_process "Skipping TLS Auth/Crypt key. $OPENVPN not found."
     echo_done
@@ -209,12 +213,21 @@ make_ta() {
   if [ -f $1 ] ; then
     echo_process "Skipping TLS Auth/Crypt key. File exists"
     echo_done
-    return 0
+  else
+    echo_process "Creating TLS Auth/Crypt key"
+    $OPENVPN --genkey secret $1 &>/dev/null
+    [ -s $1 ] && chmod 600 $1
+    echo_done
   fi
-  echo_process "Creating TLS Auth/Crypt key"
-  $OPENVPN --genkey --secret $1 &>/dev/null
-  [ -s $1 ] && chmod 600 $1
-  echo_done
+  if [ -f $2 ] ; then
+    echo_process "Skipping TLS Crypt v2 server key. File exists"
+    echo_done
+  else
+    echo_process "Creating TLS Crypt v2 server key"
+    $OPENVPN --genkey tls-crypt-v2-server $2 &>/dev/null
+    [ -s $2 ] && chmod 644 $2
+    echo_done
+  fi
 }
 
 server() {
@@ -225,18 +238,16 @@ server() {
     return 1
   fi
   ## Create CA
-  make_cert $CA_KEY $CA_CRT $CA_DAYS $RSA_BITS "$CA_CN" ca
+  make_cert $CA_KEY $CA_CRT $CA_DAYS $RSA_BITS "$CA_CN" $DGST_ALG ca
   ## Create server csr
   [ -z "$CN" ] && CN="OpenVPN Server"
-  make_cert $SERVER_KEY server.csr $CERT_DAYS $RSA_BITS "$CN"
+  make_cert $SERVER_KEY server.csr $CERT_DAYS $RSA_BITS "$CN" $DGST_ALG
   ## Sign server csr
-  sign_cert $CA_KEY $CA_CRT server.csr $SERVER_CRT $CERT_DAYS server
+  sign_cert $CA_KEY $CA_CRT server.csr $SERVER_CRT $CERT_DAYS $DGST_ALG server
   ## Create DH param
   make_dh $DH_BITS
-  ## Create TLS Auth key
-  make_ta $TA_KEY
-
-  [[ -f "$ECPARAM" ]] && rm -f "$CRT_PATH_SRV/$ECPARAM"
+  ## Create TLS Auth/Crypt key and TLS Crypt v2 server key
+  make_ta_tc2 $TA_KEY $STC2_KEY
 }
 
 client_csr() {
@@ -247,9 +258,7 @@ client_csr() {
     return 1
   fi
   [ -z "$CN" ] && func_help
-  make_cert $CLIENT_KEY client.csr $CERT_DAYS $RSA_BITS "$CN"
-
-  [[ -f "$ECPARAM" ]] && rm -f "$CRT_PATH_X/$ECPARAM"
+  make_cert $CLIENT_KEY client.csr $CERT_DAYS $RSA_BITS "$CN" $DGST_ALG
 }
 
 client_sign() {
@@ -267,7 +276,7 @@ client_sign() {
     echo "Error: $CSR_PATH - file not found" >&2
     return 1
   fi
-  sign_cert $CA_KEY $CA_CRT $CSR_PATH ${CSR_PATH%.*}.crt $CERT_DAYS client
+  sign_cert $CA_KEY $CA_CRT $CSR_PATH ${CSR_PATH%.*}.crt $CERT_DAYS $DGST_ALG client
 }
 
 client() {

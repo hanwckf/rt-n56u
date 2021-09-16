42ed7bbc5a38c8ec0f7accd30a593a07426385f0
MartinMa-SH <66724349+MartinMa-SH@users.noreply.github.com>
2021-09-16 19:01:19 +0800

    Update mtd_storage.sh

--- trunk/user/scripts/mtd_storage.sh
+++ trunk/user/scripts/mtd_storage.sh
@@ -270,7 +270,6 @@ sync && echo 3 > /proc/sys/vm/drop_caches
 #iwpriv ra0 set AssocReqRssiThres=-80
 #iwpriv rai0 set KickStaRssiLow=-85
 #iwpriv rai0 set AssocReqRssiThres=-80
->>>>>>> a321e6940bb0cb44619e21b8b3df6e91f892751a
 
 # Mount SATA disk
 #mdev -s
@@ -279,13 +278,81 @@ sync && echo 3 > /proc/sys/vm/drop_caches
 #wing 192.168.1.9:1080
 #ipset add gfwlist 8.8.4.4
 
+# 准备打印机热插拔时安装固件(从网上下载固件)
+cat > "/var/usblp_hotplug.sh" <<-\EOF
+#!/bin/sh
+set -e
+HPLJSITE=http://oleg.wl500g.info/hplj
+LOGFILE=/var/usblp_hotplug.log
+FIRMWARE=
+if [ $# -eq 3 ]; then
+    #这里用于开机时调用
+    DEVNAME=$1
+    ACTION=$2
+    DEVD=$3/../../..
+else
+    DEVD=/sys${DEVPATH}/../../..
+fi
+if [ -f $DEVD/product ]; then
+    product=`cat $DEVD/product`
+    vid=`cat $DEVD/idVendor`
+    pid=`cat $DEVD/idProduct`
+    case $vid-$pid in
+    03f0-0517)
+        FIRMWARE=sihp1000.dl
+        ;;
+    03f0-1317)
+        FIRMWARE=sihp1005.dl
+        ;;
+    03f0-4117)
+        FIRMWARE=sihp1018.dl
+        ;;
+    03f0-2b17)
+        FIRMWARE=sihp1020.dl
+        ;;
+    03f0-3d17)
+        FIRMWARE=sihpP1005.dl
+        ;;
+    03f0-3e17)
+        FIRMWARE=sihpP1006.dl
+        ;;
+    03f0-4817)
+        FIRMWARE=sihpP1005.dl
+        ;;
+    03f0-4917)
+        FIRMWARE=sihpP1006.dl
+        ;;
+    03f0-3f17)
+        FIRMWARE=sihpP1505.dl
+        ;;
+    esac
+    if [ $FIRMWARE ]; then
+        if [ ! -f /etc/storage/$FIRMWARE ]; then
+            curl -o /etc/storage/$FIRMWARE $HPLJSITE/$FIRMWARE
+        fi
+        if [ -c /dev/$DEVNAME -a $ACTION = 'add' ]; then
+           echo "$(date "+%Y-%m-%d %H:%M:%S") : Sending $product firmware to $DEVNAME" > $LOGFILE
+           cat /etc/storage/$FIRMWARE > /dev/$DEVNAME
+           echo "$(date "+%Y-%m-%d %H:%M:%S") : done." >> $LOGFILE
+        fi
+    fi
+fi
+#调用Padavan原处理程序
+if [ $# -eq 2 ]; then
+  /sbin/mdev_lp $MDEV $ACTION
+fi
 EOF
-#
-# HotPlug USB Printer HP 1020
-#
+chmod a+x /var/usblp_hotplug.sh
+sed -i 's/\/sbin\/mdev_lp/\/var\/usblp_hotplug.sh/' /etc/mdev.conf
+# 启动时如果检查到了打印机，就安装固件
+if [ -c /dev/usb/lp0 ]; then
+    devpath=`find /sys/devices/platform/ehci-platform/ -name 'lp0' | grep 'usb/lp0'`
+    if [ $? = 0 ]; then
+        /var/usblp_hotplug.sh usb/lp0 add $devpath
+    fi
+fi
 
-cat $dir_storage/turboYI_mdev_usb_lp.sh >> "$script_started"
-
+EOF
 		chmod 755 "$script_started"
 	fi
 

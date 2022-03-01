a6efa1573e385574afaf35335f3c5f898834d2fa
你家臭狗屎 <42485691+AutoCONFIG@users.noreply.github.com>
2022-03-01 11:43:56 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -152,7 +152,7 @@ typedef u_int8_t u8;
 #define EVT_RESTART_SS_TUNNEL		2
 #define EVT_RESTART_DNSFORWARDER	1
 #define EVT_RESTART_MENTOHUST		2
-#define EVT_RESTART_MINIEAP			2
+#define EVT_RESTART_MINIEAP		2
 #define EVT_RESTART_REBOOT		40
 
 struct variable

7eed69bdf5b3ea54098a579c25d726e36a86647f
qzydustin <44362429+qzydustin@users.noreply.github.com>
2022-07-15 21:14:50 -0700

    Update board.h

--- trunk/configs/boards/MI-NANO/board.h
+++ trunk/configs/boards/MI-NANO/board.h
@@ -11,10 +11,10 @@
 #define BOARD_GPIO_BTN_RESET	38
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
-#define BOARD_GPIO_LED_WIFI	11
-#define BOARD_GPIO_LED_POWER	44	/* 11: blue, 37: red, 44: yellow */
+#define BOARD_GPIO_LED_WIFI	44
+#define BOARD_GPIO_LED_POWER	37	/* 11: blue, 37: red, 44: yellow */
 #undef  BOARD_GPIO_LED_LAN
-#define BOARD_GPIO_LED_WAN  37
+#define BOARD_GPIO_LED_WAN  11
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
